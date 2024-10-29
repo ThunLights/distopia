b3157d01f6fab6209100122825c0bfa121d7a591
ROBOTofficial <tkata12@outlook.jp>
2024-10-29 07:06:19 -0400

    構造体チェッカーを追加

---
+++ src/lib/struct.ts
@@ -0,0 +1,23 @@
+import { z } from "zod"
+
+function structCheckerCore<T extends z.ZodRawShape, K extends z.infer<z.ZodObject<T>>>(obj: unknown, zodSchema: z.ZodObject<T>): obj is K {
+	try {
+		zodSchema.parse(obj)
+		return true
+	} catch {
+		return false
+	}
+}
+
+function structChecker<T extends z.ZodRawShape>(obj: unknown, zodSchema: z.ZodObject<T>) {
+	if (typeof obj === "object" && structCheckerCore(obj, zodSchema)) {
+		const newobj: z.infer<typeof zodSchema> = obj
+		return newobj
+	} else {
+		return null
+	}
+}
+
+export {
+	structChecker,
+}
