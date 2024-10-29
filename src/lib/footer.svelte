b801db3670a4e61f65712a896e009bf538d79c5c
ROBOTofficial <tkata12@outlook.jp>
2024-10-29 07:54:02 -0400

    フッターを追加

---
+++ src/lib/footer.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    export let fixed = false;
+</script>
+
+<main class="{ fixed ? "fixed" : "" }">
+    <p>Contact: <a href="https://x.com/thunlights">X ( @thunlights )</a> or robot@thunlights.com</p>
+    <p>Supported by <a href="https://www.thunlights.com">Team ThunLights</a></p>
+</main>
+
+<style lang="scss">
+    main {
+        background-color: #212227;
+    }
+    main p {
+        padding: 3px 5px;
+        color: #9ca3af;
+    }
+    .fixed {
+        width: 100%;
+        position: fixed;
+        bottom: 0;
+    }
+</style>
