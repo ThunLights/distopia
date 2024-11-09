f26878c05f12fa1aea306c981bda878499f2a3dc
ROBOTofficial <tkata12@outlook.jp>
2024-11-09 01:25:05 -0500

    メタとヘッダーlibを作成

---
+++ src/lib/header.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+    import "@fontsource/inter/900.css";
+    import "@fontsource/open-sans/800-italic.css";
+
+    export let title = "Distopia";
+    export let titleLink = "/";
+</script>
+
+<header>
+    <div class="header">
+        <div class="header-title">
+            <a href="{titleLink}">
+                <p>{title}</p>
+            </a>
+        </div>
+        <div class="header-other">
+            <a href="/about">
+                <p>About</p>
+            </a>
+        </div>
+    </div>
+</header>
+
+<style lang="scss">
+    header {
+        background-color: rgb(41,39,50);
+        position: sticky;
+        top: 0;
+        width: 100%;
+
+        .header-title p,
+        .header-other {
+            font-family: fantasy;
+        }
+        .header-title {
+            font-size: 30px;
+            padding: 20px;
+        }
+        div {
+            display: inline-block;
+        }
+        a {
+            text-decoration: none;
+            color: white;
+        }
+    }
+</style>
