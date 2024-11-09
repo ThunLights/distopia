5a13c459e88bbd6062f5c488359fc21b35e10a29
ROBOTofficial <tkata12@outlook.jp>
2024-11-09 01:26:02 -0500

    メインページに画像などを配置

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,36 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+    import { onMount } from "svelte";
+    import { getMeridiem } from "$lib/time";
+
+    import Meta from "$lib/meta.svelte";
+    import Header from "$lib/header.svelte";
+    import Footer from "$lib/footer.svelte";
+
+    let bgUrl = $state("");
+
+    onMount(async () => {
+        bgUrl = getMeridiem(navigator.language) === "AM" ? "/am.webp" : "/pm.webp";
+        document.body.style.setProperty("--body-background-color", "#313338")
+    })
+</script>
+
+<Meta></Meta>
+
+<Header></Header>
+<main style="--bg-url: url({bgUrl});">
+    <div class="entrance-img">
+        <img src="{bgUrl}" alt="test">
+    </div>
+</main>
+<Footer fixed={true}></Footer>
+
+<style>
+    main {
+        width: 100%;
+        overflow: hidden;
+    }
+    .entrance-img img {
+        width: 100%;
+        height: auto;
+    }
+</style>
