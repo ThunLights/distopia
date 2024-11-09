8f2941dea691252c303f27ab93da5b8160b3bafa
ROBOTofficial <tkata12@outlook.jp>
2024-11-09 01:26:02 -0500

    メインページに画像などを配置

---
+++ src/routes/+page.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+    import { getMeridiem } from "$lib/time";
+
+    import Meta from "$lib/meta.svelte";
+    import Header from "$lib/header.svelte";
+    import Footer from "$lib/footer.svelte";
+
+    let bgUrl = $state("");
+
+    onMount(async () => {
+        bgUrl = getMeridiem(navigator.language) === "AM" ? "/am.webp" : "/pm.webp";
+        document.body.style.setProperty("--body-background-color", "#313338")
+    })
+</script>
+
+<Meta></Meta>
+
+<Header></Header>
+<main style="--bg-url: url({bgUrl});">
+    <div class="entrance-img">
+        <img src="{bgUrl}" alt="test">
+    </div>
+</main>
+<Footer fixed={true}></Footer>
+
+<style>
+    main {
+        width: 100%;
+        overflow: hidden;
+    }
+    .entrance-img img {
+        width: 100%;
+        height: auto;
+    }
+</style>
