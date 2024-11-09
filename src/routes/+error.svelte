e4152aa942256dde47bcc7ff86662aacc3ad8e13
ROBOTofficial <tkata12@outlook.jp>
2024-11-09 01:24:48 -0500

    404ページを作成

---
+++ src/routes/+error.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+    import Meta from "$lib/meta.svelte";
+    import Header from "$lib/header.svelte";
+    import Footer from "$lib/footer.svelte";
+
+    const title = "ページが見つかりませんでした";
+    const description = [
+        "ページが見つかりませんでした。",
+        "URLのタイプミスなどはありませんか？",
+        "再度確認してみてください。"
+    ].join("");
+</script>
+
+<Meta
+    title={title}
+    description={description}
+/>
+
+<Header></Header>
+<main>
+    <div class="contents">
+        <p class="contents-title">404</p>
+        <p>ページが見つかりませんでした</p>
+    </div>
+</main>
+<Footer fixed={true}></Footer>
+
+<style lang="scss">
+    main {
+        width: 100%;
+    }
+    p {
+        color: rgb(153, 153, 153);
+    }
+    .contents {
+        text-align: center;
+    }
+    .contents-title {
+        font-size: 60px;
+    }
+</style>
