f5630be526214f8ad99f37a0d6e387d917aca7cd
ROBOTofficial <tkata12@outlook.jp>
2024-11-13 14:36:50 -0500

    ip parserを追加

---
+++ src/lib/ip.ts
@@ -0,0 +1,8 @@
+import type { RequestEvent } from "@sveltejs/kit"
+
+export function parseIp(e: RequestEvent): string {
+    const { headers } = e.request;
+    return headers.get("cf-connecting-ip") || headers.get("cf-pseudo-ipv4") || e.getClientAddress();
+}
+
+export const torCheck = (e: RequestEvent): boolean => e.request.headers.get("cf-ipcountry") === "T1";
