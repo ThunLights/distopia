342b0ef4cef3b84b03f3fba2f858622cf9648fe9
ROBOTofficial <tkata12@outlook.jp>
2024-11-13 06:30:05 -0500

    ボットをスタートさせるように変更

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,6 +1,12 @@
 import { errorHandling } from "$lib/error";
+import { discord } from "$lib/discord";
 
 import type { HandleServerError } from "@sveltejs/kit"
+
+async function start() {
+	await discord.bot.setEvents();
+	await discord.bot.login();
+}
 
 export const handleError = (async (input) => {
     if (input.status === 404) {
@@ -11,3 +17,5 @@ export const handleError = (async (input) => {
 	};
 	errorHandling(input);
 }) satisfies HandleServerError;
+
+await start();
