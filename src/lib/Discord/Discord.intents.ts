f15cb0a2d825ca861d897bc52a017a66800dd9e8
ROBOTofficial <tkata12@outlook.jp>
2024-11-13 06:42:56 -0500

    インテントを追加

---
+++ src/lib/Discord/Discord.intents.ts
@@ -0,0 +1,9 @@
+import { GatewayIntentBits } from "discord.js";
+
+export const INTENTS = [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildVoiceStates,
+];
