9883cceb70773f638ecc61259828dcbb296b59f6
ROBOTofficial <tkata12@outlook.jp>
2024-11-15 23:23:57 -0500

    認証情報を取ってくるメゾットを追加

--- src/routes/api/auth/+server.ts
+++ src/routes/api/auth/+server.ts
@@ -1,9 +1,31 @@
-import { json, type RequestHandler } from "@sveltejs/kit";
+import { json } from "@sveltejs/kit";
+import { z } from "zod";
 
-export type Response = {}
+import { structChecker } from "$lib/struct";
+import { authorization } from "$lib/auth";
+import { ServerError } from "$lib/error";
+import { database } from "$lib/Database";
+
+import type { RequestHandler } from "@sveltejs/kit";
+
+export const _ResponseZod = z.object({
+    content: z.string().or(z.object({
+        id: z.string(),
+        username: z.string(),
+        email: z.string().nullable(),
+        avatar: z.string().nullable(),
+    }))
+});
+
+export type Response = z.infer<typeof _ResponseZod>;
 
 export const POST = (async (e) => {
-    e.request.body
-    const response = {} satisfies Response;
+    const user = await authorization(e);
+    if (user instanceof ServerError) {
+        return json({}, { status: 400 })
+    }
+    const response = {
+        content: user.content,
+    } satisfies Response;
     return json(response)
 }) satisfies RequestHandler;
