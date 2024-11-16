ab2f1437917ad6bb60f1f992d7c8b1c00c644fb6
ROBOTofficial <tkata12@outlook.jp>
2024-11-16 00:15:55 -0500

    bodyの型の場所を移動

---
+++ src/routes/api/guilds/+server.ts
@@ -0,0 +1,30 @@
+import { json } from "@sveltejs/kit";
+
+import { structChecker } from "$lib/struct";
+import { authorization } from "$lib/auth";
+import { ServerError } from "$lib/error";
+import { discord } from "$lib/discord";
+import { _RequestZod } from "$lib/api/guilds/index";
+
+import type { RequestHandler } from "@sveltejs/kit";
+import type { Response } from "$lib/api/guilds/index";
+
+export const POST = (async (e) => {
+    const body = structChecker(e.request.body, _RequestZod);
+    const user = await authorization(e);
+    if (!body) {
+        return json({
+            content: "BODY_PARSE_ERROR"
+        } satisfies Response, { status: 400 })
+    }
+    if (user instanceof ServerError) {
+        return json({
+            content: user.content,
+        } satisfies Response, { status: 400 });
+    }
+    await discord.guilds(user.data.accessToken);
+    const response = {
+        content: {}
+    } satisfies Response;
+    return json(response);
+}) satisfies RequestHandler;
