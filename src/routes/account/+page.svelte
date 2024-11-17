e9ef66f9de03410dc3edf1d667531b9ac9e08697
ROBOTofficial <tkata12@outlook.jp>
2024-11-17 11:01:10 -0500

    アカウント画面にプロフィールを表示

--- src/routes/account/+page.svelte
+++ src/routes/account/+page.svelte
@@ -11,6 +11,8 @@
 
     let loginData = $state<ResponseContent | null>(null);
     let loading = $state(true);
+    let registerCount = $state(0);
+    let guildsCount = $state(0);
     let title = $state("ログインしてください");
 
     onMount(async () => {
@@ -29,36 +31,63 @@
 
 <Header userData={loginData}/>
 <main>
-    {#if loginData}
-        <div class="contents">
-            <div>
-                <p class="title">ユーザー詳細</p>
-                <div>
-                    <p>{loginData.id}</p>
+    <div class="main-div">
+        {#if loginData}
+            <div class="profile">
+                <div class="profile-contents">
+                    {#if loginData.avatar}
+                        <div class="profile-content">
+                            <img class="icon" src="https://cdn.discordapp.com/avatars/{loginData.id}/{loginData.avatar}.webp" alt="">
+                        </div>
+                    {/if}
+                    <div class="profile-content">
+                        <p class="profile-title">「 {loginData.username} 」でログイン中</p>
+                        <p>ID: {loginData.id}</p>
+                        <p>登録サーバー: {registerCount}</p>
+                        <p>登録可能サーバー: {guildsCount}</p>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="contents">
-            <div>
-                <p class="title">サーバー</p>
+            <div class="contents">
                 <div>
-                    <p>登録済み</p>
-                </div>
-                <div>
-                    <p>新規登録は<a href="/guilds/new">こちら</a></p>
+                    <p class="title">サーバー</p>
+                    <div>
+                        <p>登録済み</p>
+                    </div>
+                    <div>
+                        <p>新規登録は<a href="/guilds/new">こちら</a></p>
+                    </div>
                 </div>
             </div>
-        </div>
-    {:else if !loading}
-        <div class="contents">
-            <p>上手くデータが読み込まれませんでした</p>
-            <p>再ロードしてください</p>
-        </div>
-    {/if}
+        {:else if !loading}
+            <div class="contents">
+                <p>上手くデータが読み込まれませんでした</p>
+                <p>再ロードしてください</p>
+            </div>
+        {/if}
+    </div>
 </main>
 <Footer/>
 
 <style>
+    .profile-contents {
+        margin: 15px 10px;
+    }
+    .profile-content {
+        display: inline-block;
+        margin-right: 12px;
+    }
+    .profile-title {
+        font-size: 18px;
+        margin-bottom: 5px;
+    }
+    .profile {
+        overflow: hidden;
+        background-color: rgb(40, 40, 40)
+    }
+    .profile .icon {
+        border-radius: 50%;
+    }
     .contents {
         overflow: hidden;
         display: block;
