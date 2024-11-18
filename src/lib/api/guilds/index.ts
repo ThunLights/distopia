b5d44fbf2e83b12d2e8c66871c75897cf172595c
ROBOTofficial <tkata12@outlook.jp>
2024-11-18 06:38:34 -0500

    /api/guildsの型関係を変更

--- src/lib/api/guilds/index.ts
+++ src/lib/api/guilds/index.ts
@@ -1,9 +1,10 @@
 import { z } from "zod";
 
+import { GuildsUserZod } from "$lib/server/discord";
+
 export const _RequestZod = z.object({});
 export const _ResponseZod = z.object({
-    content: z.string().or(z.object({
-    }))
+    content: z.string().or(GuildsUserZod.array())
 });
 
 export type Request = z.infer<typeof _RequestZod>;
