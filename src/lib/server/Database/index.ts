62d874c755bc2e80f83d24054451e008ea4201c3
ROBOTofficial <tkata12@outlook.jp>
2024-11-19 21:20:08 -0500

    新しく追加されたテーブルのラッパーを作成

--- src/lib/server/Database/index.ts
+++ src/lib/server/Database/index.ts
@@ -4,6 +4,7 @@ import { Token } from "./Database.token";
 import { User } from "./Database.user";
 import { Email } from "./Database.email";
 import { Avatar } from "./Database.avatar";
+import { DatabaseGuildTables } from "./Guild/index";
 
 class DatabaseClient {
     public readonly prisma = new PrismaClient();
@@ -11,6 +12,7 @@ class DatabaseClient {
     public readonly user = new User(this.prisma.user);
     public readonly email = new Email(this.prisma.email);
     public readonly avatar = new Avatar(this.prisma.avatar);
+    public readonly guildTables = new DatabaseGuildTables(this.prisma);
 
     constructor() {
     }
