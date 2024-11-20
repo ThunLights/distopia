74807c4265c7c832b486d44992f6f7ab38bebfd4
ROBOTofficial <tkata12@outlook.jp>
2024-11-20 01:50:37 -0500

    guilds/public apiとそれのリクエストの為のwrapperが完成

--- src/lib/guilds.svelte.ts
+++ src/lib/guilds.svelte.ts
@@ -1,4 +1,5 @@
 import type { Response } from "$lib/types/guilds/index";
+import type { Response as PublicGuildsResponse } from "$lib/types/guilds/public/index";
 
 export class GuildsApiError {
     constructor(public code: string) {}
@@ -25,6 +26,29 @@ export async function getGuilds(token: string) {
         return new GuildsApiError(`API_STATUS_${response.status}`);
     } catch (error) {
         console.log(error)
+        return new GuildsApiError("ERR");
+    }
+}
+
+export async function getPublicGuilds(token: string) {
+    try {
+        const response = await fetch("/api/guilds/public", {
+            method: "POST",
+            headers: {
+                Authorization: token,
+            },
+        });
+        if (response.ok) {
+            const data: PublicGuildsResponse = await response.json();
+            if (Array.isArray(data.content)) {
+                return data.content;
+            } else {
+                return new GuildsApiError(data.content);
+            }
+        }
+        return new GuildsApiError(`API_STATUS_${response.status}`);
+    } catch (error) {
+        console.log(error);
         return new GuildsApiError("ERR");
     }
 }
--- src/routes/api/guilds/public/+server.ts
+++ src/routes/api/guilds/public/+server.ts
@@ -1,7 +1,26 @@
 import { json } from "@sveltejs/kit";
 
-import type { RequestHandler } from "@sveltejs/kit";
+import { authorization } from "$lib/server/auth";
+import { ServerError } from "$lib/server/error";
+import { database, DatabaseError } from "$lib/server/Database/index";
 
-export const POST = (async () => {
-    return json({})
+import type { RequestHandler } from "@sveltejs/kit";
+import type { Response } from "$lib/types/guilds/public/index";
+
+export const POST = (async (e) => {
+    const user = await authorization(e);
+    if (user instanceof ServerError) {
+        return json({
+            content: user.content,
+        } satisfies Response, { status: 400 })
+    }
+    const guilds = await database.guildTables.guild.userGuilds(user.data);
+    if (guilds instanceof DatabaseError) {
+        return json({
+            content: guilds.content,
+        } satisfies Response, { status: 400 })
+    }
+    return json({
+        content: guilds,
+    } satisfies Response, { status: 200 })
 }) satisfies RequestHandler;
