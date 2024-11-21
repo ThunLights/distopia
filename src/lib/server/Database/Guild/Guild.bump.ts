ae010b379be16b1f1e7649c3cf520d266f819508
ROBOTofficial <tkata12@outlook.jp>
2024-11-21 07:08:55 -0500

    bumpテーブル用クラスを編集

--- src/lib/server/Database/Guild/Guild.bump.ts
+++ src/lib/server/Database/Guild/Guild.bump.ts
@@ -1,6 +1,42 @@
+import { errorHandling } from "$lib/server/error";
+
 import type { Prisma } from "@prisma/client";
 import type { DefaultArgs } from "@prisma/client/runtime/library";
 
 export class GuildBumpTable {
     constructor(private readonly table: Prisma.GuildBumpDelegate<DefaultArgs>) {}
+
+    async guilds(take: number): Promise<string[]> {
+        try {
+            const data = await this.table.findMany({ take });
+            return data.map(value => value.id);
+        } catch (error) {
+            errorHandling(error);
+            return [];
+        }
+    }
+
+    async update(id: string) {
+        try {
+            const element = await this.table.findFirst({ where: { id }});
+            if (element) {
+                await this.table.updateMany({
+                    where: { id },
+                    data: {
+                    }
+                })
+            } else {
+                await this.table.create({
+                    data: {
+                        id,
+                        time: BigInt(Date.now()),
+                    }
+                })
+            }
+            return true;
+        } catch (error) {
+            errorHandling(error);
+            return false;
+        }
+    }
 }
