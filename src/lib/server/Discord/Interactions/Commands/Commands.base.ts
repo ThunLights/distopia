8bf05bf2f77fe74fd2d5283e0ade22f351dca38a
ROBOTofficial <tkata12@outlook.jp>
2024-11-21 03:15:10 -0500

    Discordのボットに色々コマンド追加

--- src/lib/server/Discord/Interactions/Commands/Commands.base.ts
+++ src/lib/server/Discord/Interactions/Commands/Commands.base.ts
@@ -1,15 +1,28 @@
 import { Client } from "discord.js";
 
-import type { CacheType, ChatInputCommandInteraction } from "discord.js";
+import type { CacheType, ChatInputCommandInteraction, InteractionReplyOptions, MessagePayload } from "discord.js";
 
 export class CommandsError {
     constructor(public readonly content: string) {}
 }
 
 export abstract class CommandsBase {
+    public readonly commandName: string | null = null;
+
     constructor(private readonly client: Client) {}
 
-    async commands(interaction: ChatInputCommandInteraction<CacheType>): Promise<void | CommandsError> {
-        return
+    async commands(interaction: ChatInputCommandInteraction<CacheType>): Promise<string | MessagePayload | InteractionReplyOptions | CommandsError | null> {
+        return new CommandsError("Commands Not Found");
+    }
+
+    async reply(interaction: ChatInputCommandInteraction<CacheType>): Promise<void | CommandsError> {
+        const result = await this.commands(interaction);
+        if (result === null) {
+            return void await interaction.reply({ content: codeBlock(`Error: Commands Not Found`), ephemeral: true });
+        }
+        if (result instanceof CommandsError) {
+            return void await interaction.reply({ content: codeBlock(`Error: ${result.content}`), ephemeral: true });
+        }
+        return void await interaction.reply(result);
     }
 }
--- src/lib/server/Discord/Interactions/Commands/Commands.web.ts
+++ src/lib/server/Discord/Interactions/Commands/Commands.web.ts
@@ -1,13 +1,16 @@
 import { ChatInputCommandInteraction, Client } from "discord.js";
 import { CommandsBase, CommandsError } from "./Commands.base";
 
-import type { CacheType } from "discord.js";
+import type { CacheType, InteractionReplyOptions, MessagePayload } from "discord.js";
 
 export class WebCommands extends CommandsBase {
+    public commandName = "web";
+
     constructor(client: Client) {
         super(client);
     }
 
-    async commands(interaction: ChatInputCommandInteraction<CacheType>): Promise<void | CommandsError> {
+    async commands(interaction: ChatInputCommandInteraction<CacheType>): Promise<string | MessagePayload | InteractionReplyOptions | CommandsError | null> {
+        return { content: "このコマンドは準備中です。", ephemeral: true } satisfies InteractionReplyOptions;
     }
 }
