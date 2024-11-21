89b6c74123b76858ce59a0b64fa7e1b12f7ff9d9
ROBOTofficial <tkata12@outlook.jp>
2024-11-21 07:09:24 -0500

    サーバー仮登録用クラスを編集

--- src/lib/server/Database/Guild/Guild.tmp.ts
+++ src/lib/server/Database/Guild/Guild.tmp.ts
@@ -1,6 +1,45 @@
+import { errorHandling } from "$lib/server/error";
+
 import type { Prisma } from "@prisma/client";
 import type { DefaultArgs } from "@prisma/client/runtime/library";
 
+export type GuildElement = {
+    guildId: string
+    userId: string
+    name: string
+    invite: string
+    icon?: string
+    banner?: string
+};
+
 export class GuildInviteTempTable {
     constructor(private readonly table: Prisma.GuildTmpDelegate<DefaultArgs>) {}
+
+    async data(guildId: string) {
+        try {
+            return await this.table.findFirst({ where: { guildId } });
+        } catch (error) {
+            errorHandling(error);
+            return null;
+        }
+    }
+
+    async update(guild: GuildElement) {
+        try {
+            const { guildId } = guild;
+            const element = await this.table.findFirst({ where: { guildId } })
+            if (element) {
+                await this.table.updateMany({
+                    where: { guildId },
+                    data: guild,
+                })
+            } else {
+                await this.table.create({ data: guild });
+            }
+            return true;
+        } catch (error) {
+            errorHandling(error);
+            return false;
+        }
+    }
 }
