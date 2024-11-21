64fbf0a634cd9df51e0e988d7536770df8af36e4
ROBOTofficial <tkata12@outlook.jp>
2024-11-21 07:09:05 -0500

    guild tagテーブル用クラスを編集

--- src/lib/server/Database/Guild/Guild.tag.ts
+++ src/lib/server/Database/Guild/Guild.tag.ts
@@ -1,6 +1,44 @@
-import type { Prisma } from "@prisma/client";
+import { deDepulication } from "$lib/array";
+import { errorHandling, ServerError } from "$lib/server/error";
+
+import { Prisma } from "@prisma/client";
 import type { DefaultArgs } from "@prisma/client/runtime/library";
 
 export class GuildTagTable {
     constructor(private readonly table: Prisma.GuildTagDelegate<DefaultArgs>) {}
+
+    async data(guildId: string): Promise<string[]> {
+        try {
+            return (await this.table.findMany({ where: { guildId } })).map(value => value.tag);
+        } catch (error) {
+            errorHandling(error);
+            return []
+        }
+    }
+
+    async update(guildId: string, guildTags: string[]) {
+        try {
+            const tags = deDepulication(guildTags);
+            await this.table.deleteMany({
+                where: {
+                    guildId,
+                }
+            })
+            for (const tag of tags) {
+                await this.table.create({
+                    data: {
+                        guildId,
+                        tag,
+                    }
+                })
+            }
+            return {
+                guildId,
+                tags,
+            }
+        } catch (error) {
+            errorHandling(error);
+            return new ServerError("ERROR")
+        }
+    }
 }
