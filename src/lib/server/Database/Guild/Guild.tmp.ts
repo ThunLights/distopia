f2ebe41aa585d0624ce3a80197c03a96c8d22a9b
ROBOTofficial <tkata12@outlook.jp>
2024-11-21 04:41:37 -0500

    サーバー仮登録用のテーブルを変更

--- src/lib/server/Database/Guild/Guild.tmp.ts
+++ src/lib/server/Database/Guild/Guild.tmp.ts
@@ -2,5 +2,5 @@ import type { Prisma } from "@prisma/client";
 import type { DefaultArgs } from "@prisma/client/runtime/library";
 
 export class GuildInviteTempTable {
-    constructor(private readonly table: Prisma.GuildInviteTempDelegate<DefaultArgs>) {}
+    constructor(private readonly table: Prisma.GuildTmpDelegate<DefaultArgs>) {}
 }
