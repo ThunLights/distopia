f54e32494bb1ded42a449fcb377f6ada163f9230
ROBOTofficial <tkata12@outlook.jp>
2024-11-21 01:10:58 -0500

    wrapperを作成

---
+++ src/lib/server/Database/Guild/Guild.inviteTemp.ts
@@ -0,0 +1,6 @@
+import type { Prisma } from "@prisma/client";
+import type { DefaultArgs } from "@prisma/client/runtime/library";
+
+export class GuildInviteTempTable {
+    constructor(private readonly table: Prisma.GuildInviteTempDelegate<DefaultArgs>) {}
+}
