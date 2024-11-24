54ac4e9e70d72fbbda39957443373e65201f2334
ROBOTofficial <tkata12@outlook.jp>
2024-11-24 00:36:15 -0500

    サーバーのアイコンが変わった時用のイベント作成

--- src/lib/server/Discord/Discord.guilds.ts
+++ src/lib/server/Discord/Discord.guilds.ts
@@ -1,5 +1,6 @@
+import { database, DatabaseError } from "../Database";
 import { errorHandling } from "../error";
-import { Client } from "discord.js";
+import { Client, Guild } from "discord.js";
 
 export class GuildClientError {
     constructor(public readonly content: string) {
@@ -20,6 +21,21 @@ export class GuildClient {
         } catch (error) {
             errorHandling(error);
             return false;
+        }
+    }
+
+    public async guildUpdate(oldGuild: Guild, newGuild: Guild): Promise<void> {
+        const guild = await database.guildTables.guild.id2Data(oldGuild.id);
+        if (guild instanceof DatabaseError) {
+            return;
+        }
+        if (guild) {
+            if (oldGuild.icon !== newGuild.icon) {
+                await database.guildTables.guild.update({...guild, ...{ icon: newGuild.icon }})
+            }
+            if (oldGuild.banner !== newGuild.banner) {
+                await database.guildTables.guild.update({...guild, ...{ banner: newGuild.banner }})
+            }
         }
     }
 }
