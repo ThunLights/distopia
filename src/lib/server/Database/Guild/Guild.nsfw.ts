26c9a51020cf4569b51d5a2b8002d103ba29fc87
ROBOTofficial <tkata12@outlook.jp>
2024-11-24 06:38:46 -0500

    nsfwテーブルのwrapper追加

---
+++ src/lib/server/Database/Guild/Guild.nsfw.ts
@@ -0,0 +1,32 @@
+import { errorHandling } from "$lib/server/error";
+
+import type { Prisma } from "@prisma/client";
+import type { DefaultArgs } from "@prisma/client/runtime/library";
+
+export class GuildNSFWTable {
+	constructor(private readonly table: Prisma.GuildNSFWDelegate<DefaultArgs>) {}
+
+	async update(guildId: string, content: boolean) {
+		try {
+			const data = {
+				id: guildId,
+				content,
+			}
+			const element = await this.table.findFirst({ where: { id: guildId }});
+			if (element) {
+				await this.table.updateMany({
+					where: {
+						id: element.id
+					},
+					data
+				})
+			} else {
+				await this.table.create({ data });
+			}
+			return true;
+		} catch (error) {
+			errorHandling(error);
+			return false;
+		}
+	}
+}
