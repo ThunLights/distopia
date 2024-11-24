6c8b73cf8096f66f15bd35396450a148f55ee329
ROBOTofficial <tkata12@outlook.jp>
2024-11-24 00:41:29 -0500

    アカウントごとのサーバー設定ページを追加

---
+++ src/routes/account/guild/[id]/+page.server.ts
@@ -0,0 +1,8 @@
+import { database, DatabaseError } from "$lib/server/Database";
+
+import type { PageServerLoad } from "./$types";
+
+export const load = (async (e) => {
+    return {
+    }
+}) satisfies PageServerLoad;
---
+++ src/routes/account/guild/[id]/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import Meta from "$lib/meta.svelte";
+    import Header from "$lib/header.svelte";
+    import Footer from "$lib/footer.svelte";
+
+    import { onMount } from "svelte";
+
+    onMount(async () => {})
+</script>
+
+<main>
+</main>
+
+<style></style>
