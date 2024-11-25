7ae2b1eee5707c2c7068f24940aa2484f3f86687
ROBOTofficial <tkata12@outlook.jp>
2024-11-25 03:25:51 -0500

    レビュー投稿用の内部APIを作成

--- src/lib/server/Database/Guild/Guild.review.ts
+++ src/lib/server/Database/Guild/Guild.review.ts
@@ -1,6 +1,34 @@
+import { errorHandling } from "$lib/server/error";
+
 import type { Prisma } from "@prisma/client";
 import type { DefaultArgs } from "@prisma/client/runtime/library";
 
+export type Review = {
+	guildId: string
+	userId: string
+	star: number
+	content?: string
+}
+
 export class GuildReviewTable {
     constructor(private readonly table: Prisma.GuildReviewDelegate<DefaultArgs>) {}
+
+	async update(data: Review) {
+		try {
+			const where = {
+				guildId: data.guildId,
+				userId: data.userId
+			};
+			const element = await this.table.findFirst({ where });
+			if (element) {
+				await this.table.updateMany({where, data})
+			} else {
+				await this.table.create({ data });
+			}
+			return true;
+		} catch (error) {
+			errorHandling(error);
+			return false;
+		}
+	}
 }
