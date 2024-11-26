3960ab4ae8133abe1d3a54076723c04188f1e154
ROBOTofficial <tkata12@outlook.jp>
2024-11-26 04:05:07 -0500

    タグに不正な文字が含まれていないかのチェッカーを導入

--- src/lib/tag.svelte.ts
+++ src/lib/tag.svelte.ts
@@ -1,7 +1,18 @@
-import { CHARACTER_LIMIT, TAG_COUNT_LIMIT } from "./constants.svelte";
+import { blank } from "./blank.svelte";
+import { CHARACTER_LIMIT, INVALID_TAG_CHARACTOR, TAG_COUNT_LIMIT } from "./constants.svelte";
+
+export function invalidCharactorChecker(content: string): boolean {
+	const chars = content.split("");
+	for (const char of chars) {
+		if (INVALID_TAG_CHARACTOR.includes(char)) {
+			return true;
+		}
+	}
+	return false;
+}
 
 export function tagFormatCheck(content: string) {
-	return !(content.length > CHARACTER_LIMIT.tag)
+	return !(content.length > CHARACTER_LIMIT.tag) && !blank(content) && invalidCharactorChecker(content)
 }
 
 export function tagCountCheck<T>(content: T[]) {
