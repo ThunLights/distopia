09be633a479f738f89daf943c83d7a6131ff356e
ROBOTofficial <tkata12@outlook.jp>
2024-11-27 09:57:21 -0500

    レベル取得メゾットを作成

--- src/lib/server/Database/Guild/Guild.level.ts
+++ src/lib/server/Database/Guild/Guild.level.ts
@@ -1,6 +1,17 @@
+import { errorHandling } from "$lib/server/error";
+
 import type { Prisma } from "@prisma/client";
 import type { DefaultArgs } from "@prisma/client/runtime/library";
 
 export class GuildLevelTable {
     constructor(private readonly table: Prisma.GuildLevelDelegate<DefaultArgs>) {}
+
+	async data(guildId: string) {
+		try {
+			return await this.table.findFirst({ where: { guildId } });
+		} catch (error) {
+			errorHandling(error);
+			return null;
+		}
+	}
 }
