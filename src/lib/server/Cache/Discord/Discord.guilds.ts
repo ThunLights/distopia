22eb308fd7afbead5715b1074e459924bdd031d0
ROBOTofficial <tkata12@outlook.jp>
2024-11-27 07:05:54 -0500

    guilds apiのキャッシュの型変更

--- src/lib/server/Cache/Discord/Discord.guilds.ts
+++ src/lib/server/Cache/Discord/Discord.guilds.ts
@@ -2,11 +2,11 @@ import { CacheClientBase } from "../Cache.base";
 
 import type { GuildsUser } from "$lib/server/discord";
 
-export class CacheGuilds extends CacheClientBase<GuildsUser> {
-    public caches: Record<string, GuildsUser> = {};
+export class CacheGuilds extends CacheClientBase<GuildsUser[]> {
+    public caches: Record<string, GuildsUser[]> = {};
     public deleteRate = 5 * 60 * 1000;
 
-    public insert(userId: string, data: GuildsUser): void {
+    public insert(userId: string, data: GuildsUser[]): void {
         this.caches[userId] = data;
         setTimeout(() => {
             delete this.caches[userId]
