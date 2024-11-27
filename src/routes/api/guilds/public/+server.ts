02468b809f94127a0619a9792997098077110e87
ROBOTofficial <tkata12@outlook.jp>
2024-11-27 08:05:52 -0500

    public guildsも表示できるように変更

--- src/routes/api/guilds/public/+server.ts
+++ src/routes/api/guilds/public/+server.ts
@@ -3,20 +3,25 @@ import { json } from "@sveltejs/kit";
 import { authorization } from "$lib/server/auth";
 import { ServerError } from "$lib/server/error";
 import { database, DatabaseError } from "$lib/server/Database/index";
+import { discord } from "$lib/server/discord";
 
 import type { RequestHandler } from "@sveltejs/kit";
 
+export type SuccessResponse = {
+	name: string
+	icon: string | null
+	banner: string | null
+	invite: string
+	guildId: string
+	userId: string
+	category: string
+	description: string
+	members: number | null
+	online: number | null
+}
+
 export type Response = {
-    content: string | {
-        name: string
-        icon: string | null
-        banner: string | null
-        invite: string
-        guildId: string
-        userId: string
-        category: string
-        description: string
-    }[]
+    content: string | SuccessResponse[];
 }
 
 export const POST = (async (e) => {
@@ -26,13 +31,20 @@ export const POST = (async (e) => {
             content: user.content,
         } satisfies Response, { status: 400 })
     }
+	const result = [];
     const guilds = await database.guildTables.guild.userGuilds(user.data);
     if (guilds instanceof DatabaseError) {
         return json({
             content: guilds.content,
         } satisfies Response, { status: 400 })
     }
+	for (const guild of guilds) {
+		result.push({
+			...guild,
+			...{ members: await discord.bot.control.guild.memberCount(guild.guildId), online: await discord.bot.control.guild.memberCount(guild.guildId, "online")}
+		})
+	}
     return json({
-        content: guilds,
+        content: result,
     } satisfies Response, { status: 200 })
 }) satisfies RequestHandler;
