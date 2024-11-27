05d1e07b279c818612ded331fbfc4626be2f23db
ROBOTofficial <tkata12@outlook.jp>
2024-11-27 08:01:38 -0500

    インテンツを変更

--- src/lib/server/Discord/Discord.intents.ts
+++ src/lib/server/Discord/Discord.intents.ts
@@ -6,4 +6,6 @@ export const INTENTS = [
     GatewayIntentBits.MessageContent,
     GatewayIntentBits.GuildMembers,
     GatewayIntentBits.GuildVoiceStates,
+	GatewayIntentBits.GuildPresences,
+	GatewayIntentBits.MessageContent,
 ];
