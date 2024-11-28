7f1c8d1aa4a8e354f95042f8d2eec19b6fb194bb
ROBOTofficial <tkata12@outlook.jp>
2024-11-28 04:49:10 -0500

    編集ページ作成

---
+++ src/routes/account/guild/[id]/edit/+page.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+	import Meta from "$lib/meta.svelte";
+	import Header from "$lib/header.svelte";
+	import Footer from "$lib/footer.svelte";
+	import Guild from "$lib/guild.svelte";
+
+	import { onMount } from "svelte";
+	import { getPublicGuild, GuildsApiError } from "$lib/guilds.svelte";
+
+	import type { PageData } from "./$types";
+	import type { Response } from "$routes/api/guilds/public/[id]/+server";
+
+	const { data }: { data: PageData } = $props();
+	const { guildId } = data;
+	let guild = $state<Response | null>(null);
+
+	onMount(async () => {
+		if (!data.auth) {
+			return location.href = "/account";
+		}
+		const response = await getPublicGuild(data.auth.token, guildId);
+		if (response instanceof GuildsApiError) {
+			return location.href = "/account";
+		}
+		guild = response;
+	})
+</script>
+
+<Meta/>
+
+<Header userData={data.auth}/>
+<main>
+	{#if guild}
+		<Guild
+			token={data.auth ? data.auth.token : ""}
+			method={"PATCH"}
+			guild={guild}
+		/>
+	{/if}
+</main>
+<Footer/>
+
+<style></style>
