34bcc5d573cb042610f71a53de42630f6ba812ee
ROBOTofficial <tkata12@outlook.jp>
2024-11-28 04:22:02 -0500

    不要なimportを削除

--- src/lib/tags.svelte
+++ src/lib/tags.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import { CHARACTER_LIMIT, TAG_COUNT_LIMIT } from "./constants.svelte";
 	import { tagFormatCheck } from "$lib/tag.svelte";
-	import { blank } from "./blank.svelte";
 
 	type KeyUpEvent = KeyboardEvent & {
 		currentTarget: EventTarget & HTMLInputElement;
