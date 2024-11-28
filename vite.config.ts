c3207b18238186d38fd9fcbcef072d6628faec60
ROBOTofficial <tkata12@outlook.jp>
2024-11-28 02:36:46 -0500

    難読化をするように変更

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,19 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { obfuscator } from 'rollup-obfuscator';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [
+		sveltekit(),
+		obfuscator({
+            compact: true,
+            controlFlowFlattening: true,
+            controlFlowFlatteningThreshold: 1,
+			deadCodeInjection: true,
+			deadCodeInjectionThreshold: 1,
+			stringArray: true,
+			stringArrayEncoding: [ "rc4" ],
+			stringArrayThreshold: 1,
+        }),
+	]
 });
