36d4ce93080fd1772a71f7bd99240288f8595370
ROBOTofficial <tkata12@outlook.jp>
2024-11-28 03:12:08 -0500

    エラーが頻発したので難読化解除

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { obfuscator } from 'rollup-obfuscator';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [
 		sveltekit(),
-		obfuscator({
-            compact: true,
-            controlFlowFlattening: true,
-            controlFlowFlatteningThreshold: 1,
-			deadCodeInjection: true,
-			deadCodeInjectionThreshold: 1,
-			stringArray: true,
-			stringArrayEncoding: [ "rc4" ],
-			stringArrayThreshold: 1,
-        }),
 	]
 });
