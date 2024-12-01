0629ba885b5daf163ac17026f2699c8182efcc1a
ROBOTofficial <tkata12@outlook.jp>
2024-12-01 06:49:36 -0500

    メッセージによりレベルが上がるように変更

--- src/lib/server/Discord/Discord.message.ts
+++ src/lib/server/Discord/Discord.message.ts
@@ -5,13 +5,17 @@ import type { Client, Message, OmitPartialGroupDMChannel } from "discord.js";
 export class MessageClient {
     constructor(private readonly client: Client) {}
 
+	private async culcScore(x: number) {
+		return Math.ceil(Math.sqrt(x));
+	}
+
     public async create(message: OmitPartialGroupDMChannel<Message<boolean>>): Promise<void> {
 		if (!(message.guild && message.guildId)) {
 			return;
 		}
 		const guild = await database.guildTables.guild.id2Data(message.guildId);
 		if (guild && message.content.length) {
-			await database.guildTables.level.plus(message.guildId, BigInt(message.content.length));
+			await database.guildTables.level.plus(message.guildId, BigInt(await this.culcScore(message.content.length)));
 		}
     }
 }
