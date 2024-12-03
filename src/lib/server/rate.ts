7ea790d3165b12c8af37c6a653e8eb71a6e43c3a
ROBOTofficial <tkata12@outlook.jp>
2024-12-02 21:47:38 -0500

    アクティブレート算出アルゴリズムを作成

---
+++ src/lib/server/rate.ts
@@ -0,0 +1,33 @@
+
+export type RateData = {
+    newMember: number;
+    newMessage: number;
+    vcMemberSum: number;
+    vcMemberUpperTwo: number;
+    activeMember: number;
+    allMember: number;
+};
+
+class ActiveRate {
+    public readonly h1 = 25;
+    public readonly h2 = 2;
+    public readonly h3 = 3;
+    public readonly h4 = 2;
+    public readonly h5 = 1;
+    public readonly h6 = 35;
+
+    constructor() {}
+
+    public async culc(data: RateData) {
+        const { newMember, newMessage, vcMemberSum, vcMemberUpperTwo, activeMember, allMember } = data;
+        return Math.ceil((
+            newMember * this.h1 +
+            Math.log(newMessage + 1) * Math.cbrt(newMessage) * this.h2 +
+            Math.log(vcMemberSum * this.h3 + 1) * vcMemberUpperTwo * this.h4 * Math.cbrt((vcMemberSum * this.h3) / (activeMember + 1)) +
+            Math.log(activeMember + 1) * this.h5 +
+            (Math.cbrt(allMember) - allMember ** 1.1 / (activeMember + 1))
+        ) * this.h6)
+    }
+}
+
+export const activeRate = new ActiveRate();
