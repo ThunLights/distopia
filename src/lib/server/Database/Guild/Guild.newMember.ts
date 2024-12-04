2b8f9d1eb83d8138e99c36ad8ecb0d2067dc9c5f
ROBOTofficial <tkata12@outlook.jp>
2024-12-04 05:33:21 -0500

    newMemberテーブルにupdateメゾットやデータ取得メゾットを追加

--- src/lib/server/Database/Guild/Guild.newMember.ts
+++ src/lib/server/Database/Guild/Guild.newMember.ts
@@ -1,6 +1,56 @@
+import { errorHandling } from "$lib/server/error";
+import { formatDate, getThirtyDaysAgo } from "$lib/server/date";
+
 import type { Prisma } from "@prisma/client";
 import type { DefaultArgs } from "@prisma/client/runtime/library";
 
 export class GuildNewMemberTable {
 	constructor(private readonly table: Prisma.GuildNewMemberDelegate<DefaultArgs>) {}
+
+	public async update(guildId: string) {
+		try {
+			const now = new Date(formatDate(new Date()));
+			const element = await this.table.findFirst({
+				where: { guildId, date: now },
+			});
+			if (element) {
+				await this.table.updateMany({
+					where: { guildId, date: now },
+					data: {
+						guildId,
+						count: element.count + 1,
+						date: now,
+					}
+				})
+			} else {
+				await this.table.create({
+					data: {
+						guildId,
+						count: 1,
+						date: now,
+					}
+				})
+			}
+			return true;
+		} catch (error) {
+			errorHandling(error);
+			return false;
+		}
+	}
+
+	public async thirtyDays() {
+		try {
+			const thirtyDaysAgo = getThirtyDaysAgo(new Date());
+			return await this.table.findMany({
+				where: {
+					date: {
+						gte: thirtyDaysAgo,
+					}
+				}
+			})
+		} catch (error) {
+			errorHandling(error);
+			return []
+		}
+	}
 }
