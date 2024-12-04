4b2701bc5e2dedac74af11839a20ecec76581adc
ROBOTofficial <tkata12@outlook.jp>
2024-12-04 05:23:29 -0500

    日付系の管理のための関数を追加

---
+++ src/lib/server/date.ts
@@ -0,0 +1,9 @@
+
+export function formatDate(date: Date) {
+	return `${date.getUTCFullYear()}-${date.getUTCMonth()+1}-${date.getUTCDate()}`;
+}
+
+export function getThirtyDaysAgo(date: Date) {
+    date.setDate(date.getDate() - 30);
+	return date;
+}
