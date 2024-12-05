d905f4dbe38815127ada0bae654d15a64b67fb43
ROBOTofficial <tkata12@outlook.jp>
2024-12-05 12:00:45 -0500

    基礎的な部分が完成

--- src/routes/ranking/+page.server.ts
+++ src/routes/ranking/+page.server.ts
@@ -1,11 +1,47 @@
-import { database } from "$project/src/lib/server/Database";
+import { database } from "$lib/server/Database/index";
+import { id2Guild } from "$lib/server/guild";
+
+import type { Guild } from "$lib/server/guild";
 import type { PageServerLoad } from "./$types";
 
-export const load = (async () => {
-	const level = await database.guildTables.level.ranking(50);
-	const activeRate = await database.guildTables.activeRate.ranking(50);
+const searchTypes = [
+	"activeRate",
+	"level",
+] as const;
+
+export type SearchTypes = typeof searchTypes[number];
+
+function parseSearchType(content: string) {
+	for (const searchType of searchTypes) {
+		if (searchType === content) {
+			return searchType
+		}
+	}
+	return null;
+}
+
+export const load = (async (e) => {
+	const searchTypeBase = e.url.searchParams.get("type");
+	const searchType: SearchTypes = searchTypeBase ? parseSearchType(searchTypeBase) ?? "activeRate" : "activeRate";
+	const levelBase = await database.guildTables.level.ranking(50);
+	const activeRateBase = await database.guildTables.activeRate.ranking(50);
+
+	const level: Guild[] = []
+	const activeRate: Guild[] = [];
+
+	for (const element of levelBase) {
+		const data = await id2Guild(element.guildId);
+		if (typeof data === "string") continue;
+		level.push(data);
+	}
+	for (const element of activeRateBase) {
+		const data = await id2Guild(element.guildId);
+		if (typeof data === "string") continue;
+		activeRate.push(data);
+	}
 
 	return {
+		searchType,
 		level,
 		activeRate,
 	}
--- src/routes/ranking/+page.svelte
+++ src/routes/ranking/+page.svelte
@@ -1,7 +1,84 @@
 <script lang="ts">
+	import Icon from "$lib/icon.svelte";
+
+	import type { Guild } from "$lib/server/guild";
+
 	const { data } = $props();
+	const { searchType, level, activeRate } = data;
+
+	function generateEdge(rank: number) {
+		if (rank < 1) return 1;
+		if (rank < 2) return 2;
+		if (rank < 3) return 3;
+		if (rank < 10) return 10;
+		if (rank < 30) return 30;
+		return 50;
+	}
 </script>
 
-<main></main>
+<main>
+	<div class="contents">
+		<div class="context">
+			<div>
+				<p class="title">ランキング</p>
+			</div>
+			<div class="ranking">
+				{#if searchType === "activeRate"}
+					{#each activeRate as guild, i}
+						{@render generateGuild(guild, i)}
+					{/each}
+				{:else}
+					{#each level as guild}
+						{@render generateGuild(guild)}
+					{/each}
+				{/if}
+			</div>
+		</div>
+	</div>
+</main>
 
-<style></style>
+{#snippet generateGuild(guild: Guild, rank?: number)}
+	<div class="guild">
+		<div>
+			{#if rank === undefined}
+				<img src={`https://cdn.discordapp.com/icons/${guild.guildId}/${guild.icon}.webp`} alt="">
+			{:else}
+				<Icon imgStyle="" iconPath={`https://cdn.discordapp.com/icons/${guild.guildId}/${guild.icon}.webp`} edgePath={`/ranking/${generateEdge(rank)}.webp`}/>
+			{/if}
+		</div>
+		<div>
+			<p class="name">{guild.name}</p>
+			<div class="informations">
+				<p>Lv.{guild.level ? guild.level.level : 0}</p>
+			</div>
+		</div>
+		<div>
+			<p><a href="/guilds/{guild.guildId}">詳細</a></p>
+		</div>
+	</div>
+{/snippet}
+
+<style>
+	.title {
+		font-size: 22px;
+		font-weight: 700;
+		margin-top: 10px;
+	}
+	.context {
+		overflow: hidden;
+		margin: 10px 20px;
+	}
+    .contents {
+        overflow: hidden;
+        display: block;
+        background-color: rgb(37, 36, 41);
+        border-radius: 10px;
+        width: 90%;
+        margin: 20px auto;
+    }
+	a, p {
+		font-size: 15px;
+		text-decoration: none;
+		color: white;
+	}
+</style>
