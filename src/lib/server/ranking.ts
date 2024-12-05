07780bfc11df8d30e85d90db8c81e94032fd27fd
ROBOTofficial <tkata12@outlook.jp>
2024-12-05 00:49:14 -0500

    ランキング算出用の関数作成

---
+++ src/lib/server/ranking.ts
@@ -0,0 +1,23 @@
+import { errorHandling } from "./error";
+
+export type RankingElement = {
+	guildId: string
+	content: bigint
+}
+
+export type RankingElements = Array<RankingElement>;
+
+export function parseRanking(guildId: string, elements: RankingElements) {
+	try {
+		for (let i = 0; i < elements.length; i++) {
+			const element = elements[i];
+			if (element.guildId === guildId) {
+				return i + 1;
+			}
+		}
+		return null;
+	} catch (error) {
+		errorHandling(error);
+		return null;
+	}
+}
