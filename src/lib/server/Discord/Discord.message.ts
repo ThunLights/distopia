c8046bba97ba4b627c92fde4badda5cd998503b7
ROBOT <tkata12@outlook.jp>
2024-12-07 15:39:36 +0900

    メッセージやギルドで過度な同一ユーザーによるメッセージ送信を適応しないように変更

--- src/lib/server/Discord/Discord.message.ts
+++ src/lib/server/Discord/Discord.message.ts
@@ -3,6 +3,8 @@ import { database, DatabaseError } from "../Database/index";
 import type { Client, Message, OmitPartialGroupDMChannel } from "discord.js";
 
 export class MessageClient {
+	private lateLimits: string[] = [];
+
     constructor(private readonly client: Client) {}
 
 	private async calcScore(x: number) {
@@ -20,11 +22,15 @@ export class MessageClient {
 		if (guild instanceof DatabaseError) {
 			return;
 		}
-		if (guild) {
+		if (guild && !this.lateLimits.includes(message.author.id)) {
+			this.lateLimits.push(message.author.id);
 			await database.guildTables.newMessage.update(guild.guildId);
 			if (message.content.length) {
 				await database.guildTables.level.plus(message.guildId, BigInt(await this.calcScore(message.content.length)));
 			}
+			setTimeout(() => {
+				this.lateLimits = this.lateLimits.filter(value => value !== message.author.id);
+			}, 15 * 1000);
 		}
     }
 }
