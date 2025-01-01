08631bb95aac3c5d9452021872de10d3f8afb574
ROBOT <tkata12@outlook.jp>
2025-01-01 09:19:26 +0900

    @sveltejs/enhanced-imgを追加

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { enhancedImages } from "@sveltejs/enhanced-img";
 import { obfuscator } from "rollup-obfuscator";
 import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [
+		enhancedImages(),
 		sveltekit(),
 		obfuscator(),
 	]
