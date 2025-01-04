9cde0ead30bc7b38c561b48879998626ff53b56c
ROBOT <tkata12@outlook.jp>
2025-01-04 18:32:53 +0900

    OauthFetchの大部分を排除

--- src/lib/server/Discord/Oauth/Guild/index.ts
+++ src/lib/server/Discord/Oauth/Guild/index.ts
@@ -1,13 +1,7 @@
 import { GuildJoin } from "./Guild.join";
 import { Guilds } from "./Guild.guilds";
 
-import type { OauthFetch } from "../Oauth.fetch";
-
 export class OauthGuild {
     public readonly join  = new GuildJoin();
-    public readonly guilds: Guilds;
-
-    constructor (private readonly fetch: OauthFetch) {
-        this.guilds = new Guilds(this.fetch);
-    }
+    public readonly guilds = new Guilds();
 }
