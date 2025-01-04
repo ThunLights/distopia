b4c67c5cfe8e80ab2836b56da630f8af1d89560a
ROBOT <tkata12@outlook.jp>
2025-01-04 18:25:22 +0900

    /api/auth/joinでoriginalFetchを使わないように変更

--- src/lib/server/Discord/Oauth/Guild/index.ts
+++ src/lib/server/Discord/Oauth/Guild/index.ts
@@ -4,11 +4,10 @@ import { Guilds } from "./Guild.guilds";
 import type { OauthFetch } from "../Oauth.fetch";
 
 export class OauthGuild {
-    public readonly join: GuildJoin;
+    public readonly join  = new GuildJoin();
     public readonly guilds: Guilds;
 
     constructor (private readonly fetch: OauthFetch) {
-        this.join = new GuildJoin(this.fetch);
         this.guilds = new Guilds(this.fetch);
     }
 }
