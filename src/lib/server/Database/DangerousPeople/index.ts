57b4e6cb85bc211b724ae566306a999ee936b77b
ROBOT <tkata12@outlook.jp>
2025-01-09 19:52:24 +0900

    /peopleページを作成

--- src/lib/server/Database/DangerousPeople/index.ts
+++ src/lib/server/Database/DangerousPeople/index.ts
@@ -1,3 +1,4 @@
+import { errorHandling } from "$lib/server/error";
 import { DangerousPeopleTag } from "./DangerousPeople.tag";
 
 import type { Prisma, PrismaClient } from "@prisma/client";
@@ -6,10 +7,34 @@ import type { DefaultArgs } from "@prisma/client/runtime/library";
 export class DangerousPeople {
 	public readonly tag: DangerousPeopleTag;
 
-	private readonly table: Prisma.FriendDelegate<DefaultArgs>
+	private readonly table: Prisma.DangerousPeopleDelegate<DefaultArgs>
 
 	constructor(prisma: PrismaClient) {
-		this.table = prisma.friend;
+		this.table = prisma.dangerousPeople;
 		this.tag = new DangerousPeopleTag(prisma.dangerousPeopleTag);
+	}
+
+	public async countAll() {
+		try {
+			return (await this.table.findMany()).length;
+		} catch (error) {
+			errorHandling(error);
+			return 0;
+		}
+	}
+
+	public async findMany(skip?: number) {
+		try {
+			return await this.table.findMany({
+				orderBy: {
+					time: "desc"
+				},
+				take: 50,
+				skip: skip ?? 0,
+			});
+		} catch (error) {
+			errorHandling(error);
+			return [];
+		}
 	}
 }
