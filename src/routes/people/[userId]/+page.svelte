269be75042329bf8f78a3754e49236e629db8235
ROBOT <tkata12@outlook.jp>
2025-01-10 16:25:46 +0900

    UIを調整

--- src/routes/people/[userId]/+page.svelte
+++ src/routes/people/[userId]/+page.svelte
@@ -1,7 +1,51 @@
 <script lang="ts">
+	import Footer from "$lib/footer.svelte";
+
+	import { onMount } from "svelte";
+
 	const { data } = $props();
+	const { userId, user } = data;
+
+	onMount(async () => {})
 </script>
 
-<main></main>
+<main>
+	<div class="contents">
+		<div class="context">
+			{#if user}
+				<div></div>
+			{:else}
+				<p class="title center">見つかりませんでした。</p>
+			{/if}
+		</div>
+	</div>
+</main>
+<Footer></Footer>
 
-<style></style>
+<style>
+	main {
+		min-height: 90vh;
+	}
+	.context {
+		overflow: hidden;
+		margin: 10px 20px;
+	}
+	.contents {
+		overflow: hidden;
+		display: block;
+		background-color: rgb(37, 36, 41);
+		border-radius: 10px;
+		width: 90%;
+		margin: 20px auto;
+	}
+	.title {
+		font-size: 26px;
+	}
+	.center {
+		text-align: center;
+	}
+
+	p {
+		color: white;
+	}
+</style>
