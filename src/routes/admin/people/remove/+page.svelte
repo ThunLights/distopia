5744e71d6be9b6a4a201db4b4cb5af4d9776f378
ROBOT <tkata12@outlook.jp>
2025-01-10 22:10:51 +0900

    管理パネルに削除用のページを作成

--- src/routes/admin/people/remove/+page.svelte
+++ src/routes/admin/people/remove/+page.svelte
@@ -1,23 +1,54 @@
 <script lang="ts">
 	import { PUBLIC_OWNER_ID } from "$env/static/public";
+	import { Toast } from "$lib/toast";
 	import { onMount } from "svelte";
 
 	const { data } = $props();
+
+	let targetId = $state("");
 
 	onMount(async () => {
 		if (!(data.auth && data.auth.id === PUBLIC_OWNER_ID)) {
 			return location.href = "/";
 		}
-	})
+	});
+
+	async function send() {
+		if (!data.auth) {
+			Toast.error("認証情報エラー")
+			return;
+		}
+		const response = await fetch(`/admin/api/people/${targetId}`, {
+			method: "DELETE",
+			headers: {
+				Authorization: data.auth.token,
+			}
+		});
+		if (response.ok) {
+			Toast.success(`${targetId}を削除しました。`)
+		} else {
+			try {
+				const json = await response.json();
+				Toast.error(`レスポンス「${json.content}」`)
+			} catch {
+				Toast.error("エラー")
+			}
+		}
+		targetId = "";
+	}
 </script>
 
 <main>
 	<div class="contents">
 		<div class="context">
 			<div>
-				<p>どのような操作をしますか？</p>
+				<p>どのIDを消去しますか？</p>
 			</div>
 			<div>
+				<input type="text" bind:value={targetId}>
+			</div>
+			<div>
+				<button onclick={send}>削除</button>
 			</div>
 		</div>
 	</div>
