40500b3a86ba8c5174f5cb8a13212e531c68d59c
ROBOT <tkata12@outlook.jp>
2025-01-11 16:53:27 +0900

    危険人物予報システムのウェブページが完成

--- src/routes/people/search/+page.server.ts
+++ src/routes/people/search/+page.server.ts
@@ -1,5 +1,53 @@
+import { deDepulicationStructs } from "$lib/array";
+import { database } from "$lib/server/Database/index";
+import { blank } from "$lib/blank.svelte";
+
 import type { PageServerLoad } from "./$types";
+import type { Element } from "$lib/server/Database/DangerousPeople/index";
+
+export type Elements = Array<Element & { tags?: string[] }>;
 
 export const load = (async (e) => {
-	return {}
+	const searchWord = e.url.searchParams.get("content") ?? "";
+	const words = searchWord.split(/\s+/g);
+
+	if (blank(searchWord)) {
+		return {
+			searchWord: decodeURIComponent(searchWord),
+			elements: [],
+		}
+	}
+
+	let elements: Elements = [];
+
+	for (const word of words) {
+		const search = {
+			contains: word
+		};
+
+		elements = elements.concat(await database.dangerousPeople.fetch(word, { partial: true }) ?? []);
+		elements = elements.concat(await database.dangerousPeople.search({
+			name: search,
+			title: search,
+			description: search,
+		}));
+
+		for (const userId of await database.dangerousPeople.tag.search(word)) {
+			const user = await database.dangerousPeople.fetch(userId);
+			if (user) {
+				elements.push(user);
+			}
+		}
+	}
+
+	elements = deDepulicationStructs(elements);
+	for (const element of elements) {
+		const tags = await database.dangerousPeople.tag.findUserTags(element.userId);
+		element.tags = tags.map(value => value.content);
+	}
+
+	return {
+		searchWord: decodeURIComponent(searchWord),
+		elements,
+	}
 }) satisfies PageServerLoad;
