b5b6201a041b94cd14f87c5181a4d81fb3ba87ab
ROBOT <tkata12@outlook.jp>
2025-01-15 12:36:14 +0900

    invite.svelte.tsとinvite.tsに変更

--- src/routes/staff/+page.svelte
+++ src/routes/staff/+page.svelte
@@ -3,7 +3,7 @@
     import Footer from "$lib/footer.svelte";
 
     import { staffs } from "$lib/constants"
-    import { DISCORD_INVITE_LINK } from "$lib/invite.svelte";
+    import { DISCORD_INVITE_LINK } from "$lib/invite";
 
     let title = "スタッフ情報";
 </script>
