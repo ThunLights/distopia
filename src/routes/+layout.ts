80f825daa03a6edeb80fc2963e7f3ae1144fd98f
ROBOT <tkata12@outlook.jp>
2025-01-26 10:47:25 +0900

    google adsenseによって画面が遮断される問題を修正

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -4,5 +4,6 @@ import { token2data } from "$lib/auth.svelte";
 export const load = (async () => {
     return {
         auth: browser ? await token2data() : null,
+		adsense: browser ? Boolean(localStorage.getItem("adsense")) : false,
     }
 });
