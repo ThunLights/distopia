ee102b5a25187dfe6041ff332e22940b7d2ec0c7
ROBOT <tkata12@outlook.jp>
2025-01-26 10:50:26 +0900

    localstorageからsessionstorageへ変更

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -4,6 +4,6 @@ import { token2data } from "$lib/auth.svelte";
 export const load = (async () => {
     return {
         auth: browser ? await token2data() : null,
-		adsense: browser ? Boolean(localStorage.getItem("adsense")) : false,
+		adsense: browser ? Boolean(sessionStorage.getItem("adsense")) : false,
     }
 });
