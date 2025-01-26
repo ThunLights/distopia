3db98766b5fe03cb57bc0ffaba7144a2be49766a
ROBOT <tkata12@outlook.jp>
2025-01-27 04:36:32 +0900

    不要だったヘッダーを削除

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -32,7 +32,8 @@ async function start() {
 
 export const handle = (async ({event, resolve}) => {
 	const response = await resolve(event);
-	response.headers.set("cache-control", "no-cache, no-store, must-revalidate");
+	response.headers.set("Cache-Control", "no-store");
+	response.headers.set("Pragma", "no-cache");
 	return response;
 }) satisfies Handle;
 
