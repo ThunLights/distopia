cf5e9fea8948cdbe8d1c98c6592d0cfde1bf6976
ROBOT <tkata12@outlook.jp>
2025-01-28 05:10:55 +0900

    bumpNoticeRoleテーブルを追加

--- src/lib/server/Database/Guild/Settings/index.ts
+++ src/lib/server/Database/Guild/Settings/index.ts
@@ -1,5 +1,6 @@
 import { DangerousPeople } from "./DangerousPeople/index";
 import { Bump } from "./Settings.bump";
+import { BumpNoticeRole } from "./Settings.bumpNoticeRole";
 import { Owner } from "./Settings.owner";
 
 import type { PrismaClient } from "@prisma/client";
@@ -7,11 +8,13 @@ import type { PrismaClient } from "@prisma/client";
 export class DatabaseGuildSettingsTables {
 	public readonly dangerousPeople: DangerousPeople;
 	public readonly bump: Bump
+	public readonly bumpNoticeRole: BumpNoticeRole;
 	public readonly owner: Owner;
 
 	constructor(prisma: PrismaClient) {
 		this.dangerousPeople = new DangerousPeople(prisma);
 		this.bump = new Bump(prisma.bumpNotice);
+		this.bumpNoticeRole = new BumpNoticeRole(prisma.bumpNoticeRole);
 		this.owner = new Owner(prisma.actingOwner);
 	}
 }
