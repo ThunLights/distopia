5166028a47b0414f9ac8eea52bb241ade5427ce4
ROBOT <tkata12@outlook.jp>
2025-02-06 11:55:00 +0900

    イベント一覧を取得するコードを追加

---
+++ src/routes/api/events/+server.ts
@@ -0,0 +1,44 @@
+import { authorization } from "$lib/server/auth";
+import { ServerError } from "$lib/server/error";
+import { json } from "@sveltejs/kit";
+import { generateErrorJson } from "$lib/server/json";
+import { database } from "$lib/server/Database/index";
+import { discord } from "$lib/server/discord";
+
+import type { RequestHandler } from "@sveltejs/kit";
+
+export type ResponseJson = {
+	post: {
+		content: {
+			guildId: string
+			eventId: string
+			name: string
+			description: string
+			guild: {
+				name: string
+			}
+		}[]
+	}
+}
+
+export const POST = (async (e) => {
+	const result: ResponseJson["post"]["content"] = [];
+
+	for (const { guildId, eventId } of await database.eventBoost.findMany()) {
+		const data = await discord.bot.control.guild.fetchEvent(guildId, eventId);
+		if (data && data.guild) {
+			const { name, description, guild } = data;
+			result.push({
+				guildId, eventId, name,
+				description: description ?? "",
+				guild: {
+					name: guild.name,
+				}
+			});
+		}
+	}
+
+	return json({
+		content: result,
+	} satisfies ResponseJson["post"], { status: 200 });
+}) satisfies RequestHandler;
