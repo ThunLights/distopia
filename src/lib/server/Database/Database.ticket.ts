850c3ea15a52806e72e8029ad4f867e8fc54b448
ROBOT <tkata12@outlook.jp>
2025-02-07 10:20:16 +0900

    チケット情報管理用のdbを作成

---
+++ src/lib/server/Database/Database.ticket.ts
@@ -0,0 +1,46 @@
+import { errorHandling } from "$lib/server/error";
+
+import type { Prisma } from "@prisma/client";
+import type { DefaultArgs } from "@prisma/client/runtime/library";
+
+export class Ticket {
+	constructor(private readonly table: Prisma.TicketDelegate<DefaultArgs>) {}
+
+	public async update(userId: string, channelId: string) {
+		try {
+			const element = await this.table.findFirst({ where: { userId } });
+			if (element) {
+				await this.table.updateMany({
+					where: { userId },
+					data: { channelId },
+				});
+			} else {
+				await this.table.create({ data: { userId, channelId } });
+			}
+			return true;
+		} catch (error) {
+			errorHandling(error);
+			return false;
+		}
+	}
+
+	public async remove(userId: string) {
+		try {
+			return await this.table.deleteMany({
+				where: { userId },
+			});
+		} catch (error) {
+			errorHandling(error);
+			return null;
+		}
+	}
+
+	public async fetch(userId: string) {
+		try {
+			return await this.table.findFirst({ where: { userId } });
+		} catch (error) {
+			errorHandling(error);
+			return null;
+		}
+	}
+}
