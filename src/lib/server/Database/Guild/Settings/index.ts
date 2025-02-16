fade6cc6a53908ff1ad7f3f96f32c775d4706bef
ROBOT <tkata12@outlook.jp>
2025-02-16 20:56:12 +0900

    データベースに新しいテーブルを追加

--- src/lib/server/Database/Guild/Settings/index.ts
+++ src/lib/server/Database/Guild/Settings/index.ts
@@ -1,6 +1,7 @@
 import { DatabaseClient } from "../..";
 import { DangerousPeople } from "./DangerousPeople/index";
 import { Bump } from "./Settings.bump";
+import { BumpNoticeContent } from "./Settings.bumpNoticeContent";
 import { BumpNoticeRole } from "./Settings.bumpNoticeRole";
 import { Owner } from "./Settings.owner";
 
@@ -8,5 +9,6 @@ export class DatabaseGuildSettingsTables {
 	public readonly dangerousPeople = new DangerousPeople();
 	public readonly bump = new Bump(DatabaseClient._prisma.bumpNotice);
 	public readonly bumpNoticeRole = new BumpNoticeRole(DatabaseClient._prisma.bumpNoticeRole);
+	public readonly bumpNoticeContent = new BumpNoticeContent(DatabaseClient._prisma.bumpNoticeContent);
 	public readonly owner = new Owner(DatabaseClient._prisma.actingOwner);
 }
