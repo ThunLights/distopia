ac75a09037792509c7f26db85333514044768fee
ROBOT <tkata12@outlook.jp>
2025-04-10 18:58:06 +0900

    remove: tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
-import tailwindcss from "@tailwindcss/vite";
 import { sveltekit } from "@sveltejs/kit/vite";
 import { enhancedImages } from "@sveltejs/enhanced-img";
 import { obfuscator } from "rollup-obfuscator";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit(), enhancedImages(), obfuscator()]
+	plugins: [sveltekit(), enhancedImages(), obfuscator()]
 });
