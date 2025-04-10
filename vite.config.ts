414880b3b19466ee788984e41df51481900046af
ROBOT <tkata12@outlook.jp>
2025-04-10 18:12:49 +0900

    add: tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
-import { sveltekit } from '@sveltejs/kit/vite';
+import tailwindcss from "@tailwindcss/vite";
+import { sveltekit } from "@sveltejs/kit/vite";
 import { enhancedImages } from "@sveltejs/enhanced-img";
 import { obfuscator } from "rollup-obfuscator";
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
 
 export default defineConfig({
 	plugins: [
-		enhancedImages(),
+		tailwindcss(),
 		sveltekit(),
+		enhancedImages(),
 		obfuscator(),
 	]
 });
