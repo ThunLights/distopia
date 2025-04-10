46b32fa53fc733a46ae01bcfb044c3e2c6111aa0
ROBOT <tkata12@outlook.jp>
2025-04-10 19:02:04 +0900

    remove: obfuscator

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import { enhancedImages } from "@sveltejs/enhanced-img";
-import { obfuscator } from "rollup-obfuscator";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-	plugins: [sveltekit(), enhancedImages(), obfuscator()]
+	plugins: [sveltekit(), enhancedImages()]
 });
