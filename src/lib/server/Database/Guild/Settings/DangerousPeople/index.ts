3cada69c08e7a38204b1e2a42772976073fb7024
ROBOT <tkata12@outlook.jp>
2025-05-01 14:33:45 +0900

    remove: guild tables wrapper

--- src/lib/server/Database/Guild/Settings/DangerousPeople/index.ts
+++
@@ -1,8 +0,0 @@
-import { DatabaseClient } from "$lib/server/Database/index";
-import { Ban } from "./DangerousPeople.ban";
-import { Notice } from "./DangerousPeople.notice";
-
-export class DangerousPeople {
-	public readonly ban = new Ban(DatabaseClient._prisma.dangerousPeopleBanBasis);
-	public readonly notice = new Notice(DatabaseClient._prisma.dangerousPeopleNoticeChannel);
-}
