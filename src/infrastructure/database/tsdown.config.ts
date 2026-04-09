5d22bc95633538b0f5c0c0213f20065b3a4e7a7d
ROBOT <tkata12@outlook.jp>
2026-04-10 00:55:36 +0900

    remove: tsdown

--- src/infrastructure/database/tsdown.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "tsdown";
-
-export default defineConfig({
-  entry: ["src/index.ts", "src/zod/index.ts", "src/prisma.ts"],
-  dts: true,
-  format: ["esm", "cjs"],
-  outDir: "dist",
-});
--- src/infrastructure/discord/tsdown.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "tsdown";
-
-export default defineConfig({
-  entry: ["src/index.ts", "src/prelude.ts"],
-  dts: true,
-  format: ["esm", "cjs"],
-  outDir: "dist",
-});
