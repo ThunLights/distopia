0f0284c6ad21bea0dc999f80477af5335536ccb1
ROBOT <tkata12@outlook.jp>
2026-04-16 04:57:52 +0000

    add: unit test

---
+++ src/domain/service/core/src/blank.test.ts
@@ -0,0 +1,17 @@
+import { describe, expect, test } from "vitest";
+import { isBlank, isBlankSync } from "./blank";
+
+describe("isBlank", async () => {
+  test("sync", async () => {
+    expect(isBlankSync("")).toBe(true);
+    expect(isBlankSync("abc")).toBe(false);
+    expect(isBlankSync("\n\n\t")).toBe(true);
+    expect(isBlankSync(" abc ")).toBe(false);
+  });
+  test("async", async () => {
+    expect(await isBlank("")).toBe(true);
+    expect(await isBlank("abc")).toBe(false);
+    expect(await isBlank("\n\n\t")).toBe(true);
+    expect(await isBlank(" abc ")).toBe(false);
+  });
+});
