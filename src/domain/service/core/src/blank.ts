857e6a4001f32729df397ae95ca45ffab7ed4adf
ROBOT <tkata12@outlook.jp>
2026-04-17 10:54:41 +0000

    fix: isBlankSync

--- src/domain/service/core/src/blank.ts
+++ src/domain/service/core/src/blank.ts
@@ -1,10 +1,7 @@
 import { useAsync } from "./async";
 
 export function isBlankSync(content: string): boolean {
-  content = content.replaceAll("\n", "");
-  content = content.replaceAll(/\s+/g, "");
-
-  return content === "";
+  return content.trim().replaceAll("\n", "").replaceAll(/\s+/g, "") === "";
 }
 
 export const isBlank = useAsync(isBlankSync);
