56d6ac940fad2befe3d4c4245aed510f33e96233
ROBOT <tkata12@outlook.jp>
2026-04-17 10:43:15 +0000

    add: constants

---
+++ src/domain/service/core/src/constant.ts
@@ -0,0 +1,64 @@
+export type SupporterServer = {
+  name: string;
+  invite: string;
+};
+
+export type Staff = {
+  name: string;
+  description: string;
+  links: {
+    url: string;
+  }[];
+};
+
+export const supporters = [
+  {
+    name: "クリームパンと愉快な仲間たち",
+    invite: "https://discord.gg/De8T2NS74X",
+  },
+  {
+    name: "Mikan Soba Club",
+    invite: "https://discord.gg/HaFhCWS2Kk",
+  },
+  {
+    name: "Cappuccino",
+    invite: "https://discord.gg/cappuccino",
+  },
+  {
+    name: "暇人鯖",
+    invite: "https://discord.gg/hima",
+  },
+  {
+    name: "大檸檬帝国",
+    invite: "https://discord.gg/BgZddsVPMH",
+  },
+] satisfies Array<SupporterServer>;
+
+export const staffs = [
+  {
+    name: "ROBOT",
+    description: "Distopiaの代表兼創設者でDistopiaの全てのプログラムを作成",
+    links: [
+      {
+        url: "https://github.com/ROBOTofficial",
+      },
+      {
+        url: "https://twitter.com/AlwaysHarapan",
+      },
+    ],
+  },
+  {
+    name: "Sumire",
+    description: "Distopiaのイラスト担当でDistopia内の全てのイラストを作成",
+    links: [
+      {
+        url: "https://www.tiktok.com/@sumire_8691",
+      },
+      {
+        url: "https://twitter.com/sumire_8691",
+      },
+    ],
+  },
+] as const satisfies Array<Staff>;
+
+export const DISCORD_INVITE_LINK = "https://discord.gg/QWUxsxWyYv";
--- src/domain/service/core/src/index.ts
+++ src/domain/service/core/src/index.ts
@@ -1,3 +1,4 @@
 export * from "./async";
 export * from "./blank";
+export * from "./constant";
 export * from "./omit";
