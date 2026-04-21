9e1dca1446bf1c94654575511dd5716d395c065f
ROBOT <tkata12@outlook.jp>
2026-04-21 08:16:14 +0000

    add: permissions check

--- src/presentation/bot/src/EventHandler/InteractionCreateHandler/Base/CommanInteractionBase.ts
+++ src/presentation/bot/src/EventHandler/InteractionCreateHandler/Base/CommanInteractionBase.ts
@@ -1,10 +1,12 @@
-import type {
-  CommandInteraction,
-  InteractionReplyOptions,
-  MessagePayload,
-  PermissionResolvable,
+import {
+  MessageFlags,
+  type CommandInteraction,
+  type InteractionReplyOptions,
+  type MessagePayload,
+  type PermissionResolvable,
 } from "discord.js";
 
+import { codeBlock } from "../../../utils/codeblock";
 import { Base } from "./Base";
 
 export abstract class CommandInteractionBase<
@@ -15,7 +17,23 @@ export abstract class CommandInteractionBase<
   public readonly requirePermissions: PermissionResolvable[] = [];
 
   public override async run(interaction: T): Promise<R> {
+    if (
+      !interaction.guild?.members.me
+        ?.permissionsIn(interaction.channelId)
+        .has(this.requirePermissions)
+    ) {
+      return {
+        content: [
+          "このコマンドの実行には以下の権限が必要です。",
+          codeBlock(this.requirePermissions.join(" ")),
+          "権限が足りているのに実行できない場合はボット側のインテント設定が原因の可能性が高いです。",
+        ].join("\n"),
+        flags: [MessageFlags.Ephemeral],
+      } as R;
+    }
+
     const options = await this.parseOptions(interaction);
+
     return await this.exec(interaction, options);
   }
 
