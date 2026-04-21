2109ec00749b2285f5f4fa7b2285c580afbfa9cd
ROBOT <tkata12@outlook.jp>
2026-04-21 07:49:55 +0000

    add: database

--- src/presentation/web/src/lib/server/discord.ts
+++ src/presentation/web/src/lib/server/discord.ts
@@ -1,6 +1,6 @@
 import { PUBLIC_OWNER_ID } from "$env/static/public";
+import { database } from "./database";
 import { genClient } from "infra-discord";
 import { handleClient } from "presentation-bot";
-import { database } from "./database";
 
 export const client = handleClient(genClient(), { owner: { id: PUBLIC_OWNER_ID }, database });
