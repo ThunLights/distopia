97ed1fa2d74949ff4b420f4cd2a51407291e9207
ROBOT <tkata12@outlook.jp>
2026-04-21 06:52:15 +0000

    add: fetchUser

--- src/presentation/bot/src/EventHandler/InteractionCreateHandler/Base/Base.ts
+++ src/presentation/bot/src/EventHandler/InteractionCreateHandler/Base/Base.ts
@@ -1,8 +1,20 @@
 import type { AppData } from "app-core/AppData";
 import type { BaseInteraction } from "discord.js";
+import type { User } from "domain-model";
 
 export abstract class Base<T extends BaseInteraction, R = void> {
   constructor(protected readonly appData: AppData) {}
+
+  public async parseUser(interaction: T): Promise<User> {
+    return {
+      id: interaction.user.id,
+      name: interaction.user.username,
+      displayName: interaction.user.displayName,
+      globalName: interaction.user.globalName ?? undefined,
+      avatarUrl: interaction.user.avatarURL() ?? undefined,
+      bannerUrl: interaction.user.bannerURL() ?? undefined,
+    }
+  }
 
   public abstract match(interaction: T): Promise<boolean>;
 
