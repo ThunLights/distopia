308877bb4f62326059bab0fa5d5cfb83f8dc369e
ROBOT <tkata12@outlook.jp>
2026-04-25 10:20:28 +0000

    add: lint and format

--- src/infrastructure/database/src/index.ts
+++ src/infrastructure/database/src/index.ts
@@ -1,10 +1,11 @@
-import { PrismaClient } from "./prisma-client"
 import { PrismaPg } from "@prisma/adapter-pg";
 
+import { PrismaClient } from "./prisma-client";
+
 export function genPrismaClient(databaseUrl: string) {
-    return new PrismaClient({
-        adapter: new PrismaPg({
-            connectionString: databaseUrl,
-        })
-    })
+  return new PrismaClient({
+    adapter: new PrismaPg({
+      connectionString: databaseUrl,
+    }),
+  });
 }
