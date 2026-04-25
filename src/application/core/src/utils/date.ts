5d2b792d2965077d7227e7061294b500a4148553
ROBOT <tkata12@outlook.jp>
2026-04-25 09:30:17 +0000

    fix: move domain-service-core -> app-core

--- src/application/core/src/utils/date.ts
+++ src/application/core/src/utils/date.ts
@@ -1,4 +1,4 @@
-import { useAsync } from "./async";
+import { useAsync } from "domain-service-core";
 
 export function formatYMDSync(date: Date) {
   return `${date.getUTCFullYear()}/${(date.getUTCMonth() + 1).toString().padStart(2, "0")}/${date.getUTCDate().toString().padStart(2, "0")}`;
