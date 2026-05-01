5cff6f355eaae25b13d1ed77cf54cd5ae624402f
ROBOT <tkata12@outlook.jp>
2026-05-01 09:29:40 +0000

    feat: refactor discord integration to use controller directly

--- src/presentation/web/src/lib/server/core.ts
+++ src/presentation/web/src/lib/server/core.ts
@@ -1,14 +1,13 @@
 import { PUBLIC_OWNER_ID, PUBLIC_URL } from "$env/static/public";
 import { database } from "./database";
-import { client } from "./discord";
+import { controller } from "./discord";
 import { memory } from "./memory";
 import { AppCore } from "app-core";
-import { Controller } from "infra-discord";
 
 export const core = new AppCore({
   owner: { id: PUBLIC_OWNER_ID },
   url: PUBLIC_URL,
   memory,
-  discord: new Controller(client),
+  discord: controller,
   database,
 });
--- src/presentation/web/src/lib/server/discord.ts
+++ src/presentation/web/src/lib/server/discord.ts
@@ -1,5 +1,7 @@
 import { core } from "./core";
-import { genClient } from "infra-discord";
+import { Controller, genClient } from "infra-discord";
 import { handleClient } from "presentation-bot";
 
 export const client = handleClient(genClient(), core);
+
+export const controller = new Controller(client);
