1532adda7030ffeefd5056b4d5a1aaa784674bd9
ROBOT <tkata12@outlook.jp>
2026-05-03 18:39:26 +0000

    fix: transaction

--- src/application/core/src/Voice.ts
+++ src/application/core/src/Voice.ts
@@ -1,15 +1,23 @@
-import { levelUp } from "domain-service";
+import { levelUpSync } from "domain-service";
 
 import { Base } from "./Base";
 import { formatYMD } from "./utils/date";
 
+export type UpsertVcMembersQuery = {
+  guildId: string;
+  vcMember: string;
+}[];
+
+export type UpsertQuery = {
+  guildId: string;
+  plusPoint: number;
+}[];
+
 export class Voice extends Base {
   public async update() {
     const upsertVcMemberUpperTwoQuery: string[] = [];
-    const upsertVcMembersQuery: {
-      guildId: string;
-      vcMember: string;
-    }[] = [];
+    const upsertVcMembersQuery: UpsertVcMembersQuery = [];
+    const upsertQuery: UpsertQuery = [];
     const voiceChannels = await this.state.discord.channel.fetchVoiceChannel();
 
     for (const vc of voiceChannels) {
@@ -26,6 +34,27 @@ export class Voice extends Base {
       this.state.memory.voiceChannelMember.pushMemberCounts(vc.guildId, vc.activeMemberCount);
     }
 
+    for (const [guildId, value] of this.state.memory.voiceChannelMember.entries()) {
+      if (voiceChannels.map(({ guildId }) => guildId).includes(guildId)) {
+        const plusPoint =
+          value.memberCounts.reduce((sum, e) => sum + e, 0) / value.memberCounts.length;
+
+        upsertQuery.push({
+          guildId,
+          plusPoint,
+        });
+      } else {
+        this.state.memory.voiceChannelMember.delete(guildId);
+      }
+    }
+
+    const records = new Map(
+      (await this.state.database.guildRecord.findAll()).map(({ guildId, level, point }) => [
+        guildId,
+        { guildId, level, point },
+      ]),
+    );
+
     await this.state.database.guildRecordOneDay.upsertVcMemberUpperTwoAll(
       upsertVcMemberUpperTwoQuery,
       await formatYMD(new Date()),
@@ -34,23 +63,17 @@ export class Voice extends Base {
       upsertVcMembersQuery,
       await formatYMD(new Date()),
     );
-
-    for (const [guildId, value] of this.state.memory.voiceChannelMember.entries()) {
-      if (voiceChannels.map(({ guildId }) => guildId).includes(guildId)) {
-        const data = await this.state.database.guildRecord.find(guildId);
-        const plusPoint =
-          value.memberCounts.reduce((sum, e) => sum + e, 0) / value.memberCounts.length;
-
-        const { level, point } = await levelUp(
-          data?.level ?? 0n,
-          data?.point ?? 0n,
+    await this.state.database.guildRecord.upsertAll(
+      upsertQuery.map(({ guildId, plusPoint }) => {
+        const record = records.get(guildId);
+        const { level, point } = levelUpSync(
+          record?.level ?? 0n,
+          record?.point ?? 0n,
           BigInt(Math.ceil(plusPoint) * 20),
         );
 
-        return await this.state.database.guildRecord.upsert({ guildId, level, point });
-      } else {
-        this.state.memory.voiceChannelMember.delete(guildId);
-      }
-    }
+        return { guildId, level, point };
+      }),
+    );
   }
 }
