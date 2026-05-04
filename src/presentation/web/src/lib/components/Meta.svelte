d5e36b9230cb0fcb0e8442d582d4b2fe375ee9bd
ROBOT <tkata12@outlook.jp>
2026-05-04 14:29:05 +0000

    feat: add Meta

--- src/presentation/web/src/lib/components/Meta.svelte
+++ src/presentation/web/src/lib/components/Meta.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   const {
     siteName = "Distopia.top / Discordサーバー用掲示板",
-    title = "Distopia / Discordサーバー用掲示板",
+    title = "Distopia",
     keywords = ["discord", "discord server", "discordサーバー掲示板", "サーバー", "掲示板"],
     description = [
       "Distopia.topはDiscordサーバー用掲示板です。",
@@ -9,17 +9,19 @@
     ].join(""),
     url = "https://distopia.top/",
   } = $props();
+
+  const metaTitle = $derived(`${title} / Discordサーバー用掲示板`);
 </script>
 
 <svelte:head>
   <meta name="description" content={description} />
   <meta name="keywords" content={keywords.join(", ")} />
   <meta name="theme-color" content="#37b6e4" />
-  <title>{title}</title>
+  <title>{metaTitle}</title>
 
   <meta property="og:url" content={url} />
   <meta property="og:type" content="website" />
-  <meta property="og:title" content={title} />
+  <meta property="og:title" content={metaTitle} />
   <meta property="og:description" content={description} />
   <meta property="og:site_name" content={siteName} />
 
--- src/presentation/web/src/routes/+page.svelte
+++ src/presentation/web/src/routes/+page.svelte
@@ -1,2 +1,8 @@
+<script lang="ts">
+  import Meta from "$lib/components/Meta.svelte";
+</script>
+
+<Meta />
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
