7e5b11f83ed167acfdf9692d79694c4a2c57d1d5
ROBOT <tkata12@outlook.jp>
2026-05-07 09:14:39 +0000

    feat: add JWTKey and UserJWTVerifyKey to AppState and memory

--- src/application/core/src/AppState.ts
+++ src/application/core/src/AppState.ts
@@ -2,10 +2,12 @@ import type { DatabaseClient } from "infra-database/types";
 import type { Controller } from "infra-discord";
 import type { GuildEdit } from "repo-memory/GuildEdit";
 import type { GuildMemberAdd } from "repo-memory/GuildMemberAdd";
+import type { JWTKey } from "repo-memory/JWTKey";
 import type { GuildBumpLateLimit } from "repo-memory/latelimit/GuildBumpLateLimit";
 import type { MessageCreateLateLimit } from "repo-memory/latelimit/MessageCreateLateLimit";
 import type { MessageCreate } from "repo-memory/MessageCreate";
 import type { UnJoinedGuild } from "repo-memory/UnJoinedGuild";
+import type { UserJWTVerifyKey } from "repo-memory/UserJWTVerifyKey";
 import type { VoiceChannelMember } from "repo-memory/VoiceChannelMember";
 
 export type AppState = {
@@ -20,8 +22,10 @@ export type AppState = {
     };
     guildEdit: GuildEdit;
     guildMemberAdd: GuildMemberAdd;
+    jwtKey: JWTKey;
     messageCreate: MessageCreate;
     unJoinedGuild: UnJoinedGuild;
+    userJWTVerifyKey: UserJWTVerifyKey;
     voiceChannelMember: VoiceChannelMember;
   };
   discord: Controller;
--- src/presentation/web/src/lib/server/memory.ts
+++ src/presentation/web/src/lib/server/memory.ts
@@ -1,8 +1,10 @@
 import type { AppState } from "app-core/AppState";
 import { GuildEdit } from "repo-memory/GuildEdit";
 import { GuildMemberAdd } from "repo-memory/GuildMemberAdd";
+import { JWTKey } from "repo-memory/JWTKey";
 import { MessageCreate } from "repo-memory/MessageCreate";
 import { UnJoinedGuild } from "repo-memory/UnJoinedGuild";
+import { UserJWTVerifyKey } from "repo-memory/UserJWTVerifyKey";
 import { VoiceChannelMember } from "repo-memory/VoiceChannelMember";
 import { GuildBumpLateLimit } from "repo-memory/latelimit/GuildBumpLateLimit";
 import { MessageCreateLateLimit } from "repo-memory/latelimit/MessageCreateLateLimit";
@@ -14,7 +16,9 @@ export const memory: AppState["memory"] = {
   },
   guildEdit: new GuildEdit(),
   guildMemberAdd: new GuildMemberAdd(),
+  jwtKey: new JWTKey(),
   messageCreate: new MessageCreate(),
   unJoinedGuild: new UnJoinedGuild(),
+  userJWTVerifyKey: new UserJWTVerifyKey(),
   voiceChannelMember: new VoiceChannelMember(),
 };
