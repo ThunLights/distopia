02f2b90f25b85e9e1cb7668ad16f8101a550b813
ROBOT <tkata12@outlook.jp>
2026-05-08 17:14:54 +0000

    feat: add authentication page with user feedback and redirection logic

---
+++ src/presentation/web/src/routes/auth/+page.server.ts
@@ -0,0 +1,29 @@
+import { core } from "$lib/server/core";
+import { jwt } from "$lib/server/jwt";
+import type { UserAuth } from "$lib/shared/types/UserAuth";
+import type { PageServerLoad } from "./$types";
+
+export const load: PageServerLoad = async (e) => {
+  const code = e.url.searchParams.get("code");
+  let user: UserAuth | null = null;
+
+  if (code) {
+    const apiResult = await core.oauth2.codeToUser(code);
+
+    if (apiResult) {
+      const { id, username, avatarUrl } = apiResult;
+      const token = await jwt.sign({ userId: id });
+
+      if (token) {
+        user = {
+          id,
+          username,
+          avatarUrl: avatarUrl ?? undefined,
+          token,
+        };
+      }
+    }
+  }
+
+  return { user };
+};
---
+++ src/presentation/web/src/routes/auth/+page.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  import { setAuth } from "$lib/client/auth";
+  import Block from "$lib/components/Block.svelte";
+  import Meta from "$lib/components/Meta.svelte";
+  import Title from "$lib/components/Title.svelte";
+  import { onMount } from "svelte";
+
+  const { data } = $props();
+  const { user } = $derived(data);
+  const result = $derived(user ? "認証に成功しました。" : "認証に失敗しました。");
+  const title = $derived(result);
+  const description = $derived(
+    result ? `${result}数秒後にリダイレクトされます。` : `${result}再度試してみてください。`,
+  );
+
+  onMount(async () => {
+    if (user) {
+      await setAuth(user.token);
+      setTimeout(async () => {
+        location.href = "/";
+      }, 3000);
+    }
+  });
+</script>
+
+<Meta {title} {description} />
+
+<Block>
+  <Title content={result} />
+  <p>{description}</p>
+</Block>
