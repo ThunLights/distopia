d44a7b558d34d5f53d80e478871c1c61faeff825
ROBOT <tkata12@outlook.jp>
2026-05-10 07:03:01 +0000

    refactor: extract core initialization into genCore function

--- src/presentation/web/src/lib/server/core.ts
+++ src/presentation/web/src/lib/server/core.ts
@@ -3,8 +3,13 @@ import { djsController } from "./bot";
 import { database } from "./database";
 import { memory } from "./memory";
 import { AppCore } from "app-core";
+import type { AppState } from "app-core/AppState";
 
-export const core = new AppCore({
+export function genCore(state: AppState) {
+  return new AppCore(state);
+}
+
+export const core = genCore({
   owner: { id: PUBLIC_OWNER_ID },
   url: PUBLIC_URL,
   memory,
