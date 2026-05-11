237099b08b47847d47ef0c85fa4a3f56d1d128ff
ROBOT <tkata12@outlook.jp>
2026-05-11 14:04:12 +0000

    feat: ensure public guilds are considered in ranking queries

--- src/infrastructure/database/prisma/sql/getRankingLevelAll.sql
+++ src/infrastructure/database/prisma/sql/getRankingLevelAll.sql
@@ -1,5 +1,5 @@
 SELECT *
 FROM "GuildRecord" as gr
     INNER JOIN "Guild" as g
-    ON gr."guildId" = g."guildId"
+    ON gr."guildId" = g."guildId" AND g."public" IS TRUE
 ORDER BY gr."level" DESC;
