5e4aa605f239a747d7fac7b7c2692d6cce41d93d
ROBOT <tkata12@outlook.jp>
2026-05-12 18:32:13 +0000

    feat: add Review component for displaying user reviews with star ratings

---
+++ src/presentation/web/src/lib/components/Guild/Review.svelte
@@ -0,0 +1,64 @@
+<script lang="ts">
+  import BlackStar from "$lib/assets/icon/blackstar.webp";
+  import Star from "$lib/assets/icon/star.webp";
+
+  type Props = {
+    username: string | null;
+    avatarUrl: string | null;
+    star: number;
+    content: string | null;
+  };
+
+  const { username, avatarUrl, star, content }: Props = $props();
+</script>
+
+<div class="review">
+  <div class="context">
+    <div class="user">
+      <div>
+        <img class="icon" src={avatarUrl ?? "/discord.webp"} alt="" />
+      </div>
+      <div>
+        <p class="name">{username ?? "unknown user"}</p>
+      </div>
+    </div>
+    <div class="stars">
+      <div class="inline-block">
+        <p class="section-title">評価</p>
+      </div>
+      {#each [0, 1, 2, 3, 4] as i (i)}
+        <div class="star inline-block">
+          <img src={i + 1 <= star ? Star : BlackStar} alt="" />
+        </div>
+      {/each}
+    </div>
+    {#if content}
+      <div>
+        <p class="section-title">内容</p>
+        <pre>{content}</pre>
+      </div>
+    {/if}
+  </div>
+</div>
+
+<style>
+  .review {
+    overflow: hidden;
+    border-radius: 20px;
+    background-color: rgb(46, 46, 46);
+    margin-top: 12px;
+  }
+  .review .user > div {
+    display: inline-block;
+  }
+  .review .user .icon {
+    margin: 0;
+    height: 18px;
+  }
+  .review .user .name {
+    margin: 0;
+    line-height: 16px;
+    font-size: 16px;
+    font-weight: 500;
+  }
+</style>
