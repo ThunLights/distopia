5458ae69f6632f2872c6c7869f71e58cdee0d3d4
ROBOT <tkata12@outlook.jp>
2026-05-12 18:22:17 +0000

    feat: add BlockTitle component for displaying guild titles

---
+++ src/presentation/web/src/lib/components/Guild/BlockTitle.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  type Props = {
+    title: string;
+  };
+
+  const { title }: Props = $props();
+</script>
+
+<p class="block-title">{title}</p>
+
+<style>
+  .block-title {
+    font-size: 22px;
+    font-weight: 700;
+    margin-top: 10px;
+  }
+</style>
