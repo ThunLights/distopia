4866081b21d9d15a90e8d3fd13443082df27e8a5
ROBOT <tkata12@outlook.jp>
2026-05-17 13:24:37 +0000

    fix: typo

--- src/presentation/bot/src/EventHandler/InteractionCreateHandler/ChatInputCommand/FriendCommand.ts
+++ src/presentation/bot/src/EventHandler/InteractionCreateHandler/ChatInputCommand/FriendCommand.ts
@@ -52,7 +52,7 @@ export class FriendCommand extends ChatInputCommandBase<Options> {
               .setMaxLength(CHARACTER_LIMIT.description),
           ),
         new LabelBuilder()
-          .setLabel(`タグ (最大${NUM_TAG_LIMIT}}個, 最大${CHARACTER_LIMIT.tag}文字, Enterで区切り)`)
+          .setLabel(`タグ (最大${NUM_TAG_LIMIT}個, 最大${CHARACTER_LIMIT.tag}文字, Enterで区切り)`)
           .setTextInputComponent(
             new TextInputBuilder().setCustomId("tags").setStyle(TextInputStyle.Paragraph),
           ),
