00eac16c3fd94618d30a0fe5e5bef93c74b120a1
ROBOT <tkata12@outlook.jp>
2026-05-18 11:22:11 +0000

    fix: add dotenv configuration to load environment variables in Storybook

--- src/presentation/web/.storybook/main.ts
+++ src/presentation/web/.storybook/main.ts
@@ -1,7 +1,17 @@
 import type { StorybookConfig } from "@storybook/sveltekit";
-import { dirname } from "path";
+import dotenv from "dotenv";
+import { existsSync } from "fs";
+import { dirname, join } from "path";
 import { fileURLToPath } from "url";
 import { defineConfig } from "vite";
+
+const dotenvPath = join(process.cwd(), "../../../../.env");
+
+if (existsSync(dotenvPath)) {
+  dotenv.config({
+    path: dotenvPath,
+  });
+}
 
 /**
  * This function is used to resolve the absolute path of a package.
