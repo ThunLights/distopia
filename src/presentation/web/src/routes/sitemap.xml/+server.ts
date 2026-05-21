1f9f24c5263543ccacce10370881de16fb426271
EdamAmex <121654029+EdamAme-x@users.noreply.github.com>
2026-05-21 20:45:02 +0900

    Fix sitemap dynamic guild params

--- src/presentation/web/src/routes/sitemap.xml/+server.ts
+++ src/presentation/web/src/routes/sitemap.xml/+server.ts
@@ -1,10 +1,18 @@
+import { database } from "$lib/server/database";
 import type { RequestHandler } from "@sveltejs/kit";
 import * as sitemap from "super-sitemap";
 
 export const GET: RequestHandler = async () => {
+  const publicGuildIds = (await database.guild.findAll())
+    .filter(({ public: isPublic }) => isPublic)
+    .map(({ guildId }) => guildId);
+
   return await sitemap.response({
     origin: "https://distopia.top",
     excludeRoutePatterns: ["^/api.*", "^/user.*", "^/auth.*"],
+    paramValues: {
+      "/guilds/[id]": publicGuildIds,
+    },
     defaultChangefreq: "daily",
   });
 };
