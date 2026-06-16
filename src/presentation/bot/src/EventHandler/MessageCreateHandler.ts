7878a3c560b0b773631d3ccca189743e5fcd8ce5
ROBOT <tkata12@outlook.jp>
2026-06-16 19:42:40 +0000

    feat: refactor invite link handling logic for improved readability

--- src/presentation/bot/src/EventHandler/MessageCreateHandler.ts
+++ src/presentation/bot/src/EventHandler/MessageCreateHandler.ts
@@ -12,29 +12,33 @@ export class MessageCreateHandler extends BaseHandler<
       await this.core.message.increase(message.guildId, message.member.id, message.content);
     }
 
-    const inviteLinks = await this.core.message.includeInviteLink(message.content);
-    if (inviteLinks.length) {
-      if (message.deletable) {
-        await message.delete();
-      }
-      return;
-    }
-
-    const embedInviteLinks = await this.core.state.discord.embed.detectInviteLinks(message.embeds);
-    if (embedInviteLinks.length) {
-      const messages = (await message.channel.messages.fetch({ limit: 30 })).values().toArray();
-
-      for (const msg of messages) {
-        if (msg.deletable) {
-          await msg.delete();
+    if (!message.member?.permissions.has("Administrator")) {
+      const inviteLinks = await this.core.message.includeInviteLink(message.content);
+      if (inviteLinks.length) {
+        if (message.deletable) {
+          await message.delete();
         }
+        return;
       }
 
-      if (message.deletable) {
-        await message.delete();
-      }
+      const embedInviteLinks = await this.core.state.discord.embed.detectInviteLinks(
+        message.embeds,
+      );
+      if (embedInviteLinks.length) {
+        const messages = (await message.channel.messages.fetch({ limit: 30 })).values().toArray();
 
-      return;
+        for (const msg of messages) {
+          if (msg.deletable) {
+            await msg.delete();
+          }
+        }
+
+        if (message.deletable) {
+          await message.delete();
+        }
+
+        return;
+      }
     }
   }
 }
