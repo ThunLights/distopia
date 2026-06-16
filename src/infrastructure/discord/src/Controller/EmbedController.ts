29518d263433907c4a97a4bc2a5755afc02d71ba
ROBOT <tkata12@outlook.jp>
2026-06-16 17:49:41 +0000

    feat: add EmbedController for detecting Discord invite links

---
+++ src/infrastructure/discord/src/Controller/EmbedController.ts
@@ -0,0 +1,13 @@
+import type { Embed } from "discord.js";
+
+import { Base } from "./Base";
+
+export class EmbedController extends Base {
+  public async detectInviteLinks(embeds: Embed[]) {
+    const inviteLinks = embeds
+      .filter((embed) => embed.provider && embed.provider.name === "Discord")
+      .map(embed => embed.provider?.url)
+      .filter(url => url !== undefined);
+    return inviteLinks;
+  }
+}
