58cca64a2819c1052d65f113d4077cbfe768e4d1
ROBOT <tkata12@outlook.jp>
2026-06-22 22:23:03 +0000

    feat: remove AbortSignal.timeout from DEFAULT_TIMEOUT and DISCORD_TIMEOUT constants

--- src/infrastructure/http/src/timeout.ts
+++ src/infrastructure/http/src/timeout.ts
@@ -1,3 +1,3 @@
-export const DEFAULT_TIMEOUT = AbortSignal.timeout(10 * 1000); // 10 secs
+export const DEFAULT_TIMEOUT = 10 * 1000; // 10 secs
 
-export const DISCORD_TIMEOUT = AbortSignal.timeout(30 * 1000); // 30 secs
+export const DISCORD_TIMEOUT = 30 * 1000; // 30 secs
