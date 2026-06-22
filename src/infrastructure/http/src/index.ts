4541459b287a944200ed8cb23cf8406dea3c0671
ROBOT <tkata12@outlook.jp>
2026-06-22 20:41:57 +0000

    feat: add size and timeout modules for response handling

--- src/infrastructure/http/src/index.ts
+++ src/infrastructure/http/src/index.ts
@@ -2,4 +2,6 @@ export * from "./Error/LocalAddressError";
 export * from "./invite";
 export * from "./safefetch";
 export * from "./safeurl";
+export * from "./size";
+export * from "./timeout";
 export * from "./url";
---
+++ src/infrastructure/http/src/timeout.ts
@@ -0,0 +1,1 @@
+export const DEFAULT_TIMEOUT = AbortSignal.timeout(10 * 1000); // 10 secs
