4d68eb86664ba28e1711c839f9433947a18db45d
ROBOT <tkata12@outlook.jp>
2026-06-25 23:15:37 +0000

    feat: Add Meta component for server registration page

--- src/presentation/web/src/routes/help/register/+page.svelte
+++ src/presentation/web/src/routes/help/register/+page.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
   import Block from "$lib/components/Block.svelte";
   import Register from "$lib/components/Help/Register.svelte";
+  import Meta from "$lib/components/Meta.svelte";
 </script>
+
+<Meta title="サーバー登録の仕方" />
 
 <Block>
   <Register />
