7969e87d60d94ef4ce643710a14285d4cb65cf20
ROBOT <tkata12@outlook.jp>
2026-07-25 04:04:10 +0000

    feat: add check to stop existing Docker containers before starting new ones

--- production/run.sh
+++ production/run.sh
@@ -3,4 +3,9 @@
 project_dir=$(cd $(dirname $0); cd ../; pwd)
 
 cd $project_dir/docker
+
+if [ -n "$(docker compose -f docker-compose.yml -f docker-compose.prod.yml ps -q)" ]; then
+  docker compose -f docker-compose.yml -f docker-compose.prod.yml down
+fi
+
 docker compose -f docker-compose.yml -f docker-compose.prod.yml up -d
