24ca4b2a9106a68b2aac44f11c0fba527312626c
ROBOT <tkata12@outlook.jp>
2026-07-25 04:08:44 +0000

    fix(run.sh): simplify Docker container management by removing redundant compose file references

--- production/run.sh
+++ production/run.sh
@@ -4,8 +4,8 @@ project_dir=$(cd $(dirname $0); cd ../; pwd)
 
 cd $project_dir/docker
 
-if [ -n "$(docker compose -f docker-compose.yml -f docker-compose.prod.yml ps -q)" ]; then
-  docker compose -f docker-compose.yml -f docker-compose.prod.yml down
+if [ -n "$(docker compose ps -q)" ]; then
+  docker compose down
 fi
 
 docker compose -f docker-compose.yml -f docker-compose.prod.yml up -d
