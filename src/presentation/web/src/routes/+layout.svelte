9202fff038f09082754d528ce160713998ad80ef
ROBOT <tkata12@outlook.jp>
2026-08-09 11:12:41 +0000

    chore: add @qwik.dev/partytown for improved performance and script handling

--- src/presentation/web/src/routes/+layout.svelte
+++ src/presentation/web/src/routes/+layout.svelte
@@ -8,6 +8,31 @@
 </script>
 
 <SvelteToast />
+<svelte:head>
+  <script>
+    window.partytown = { forward: ["dataLayer.push"] };
+  </script>
+  <script async src="/~partytown/partytown.js"></script>
+  <script
+    async
+    fetchpriority="low"
+    src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9010324670720699"
+    crossorigin="anonymous"
+  ></script>
+  <script
+    type="text/partytown"
+    src="https://www.googletagmanager.com/gtag/js?id=G-XZSXC8MQPZ"
+  ></script>
+  <script type="text/partytown">
+    window.dataLayer = window.dataLayer || [];
+    function gtag() {
+      dataLayer.push(arguments);
+    }
+    gtag("js", new Date());
+
+    gtag("config", "G-XZSXC8MQPZ");
+  </script>
+</svelte:head>
 
 <Header userData={data.user} />
 <main>
